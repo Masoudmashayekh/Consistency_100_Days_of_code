848ae87e14ee8dc952d13a45c6153266c1ee1848
Masoud <masoudmashayekh4944@gmail.com>
2025-11-10 16:45:31 +0100

    save

--- Day1_Variables/main.py
+++ Day1_Variables/main.py
@@ -6,6 +6,10 @@ print("3. Add 3g of Salt.")
 print("4. Leave to rise for 2 hours.")
 print("5. Bake at 200 degrees C for 30 minutes.")
 
-print(f"{'\\n'} symbol is a newline character.")
-print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+# \n symbol is a newline character.
+print("Hello World!\nI'm here to be consistent in what I do.")
 
+# You can use + to combine strings
+print("Hello" + " " + "Masoud")
+
+# IndentationError: Spaces are very important in Python.
