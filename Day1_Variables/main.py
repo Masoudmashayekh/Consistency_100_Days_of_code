7c449a6bb3a693d2dc5b6d7fc761cbb7e18ef6bf
Masoud <masoudmashayekh4944@gmail.com>
2025-11-10 16:28:35 +0100

    save it

--- Day1_Variables/main.py
+++ Day1_Variables/main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+print("Hello World!") # use " " for Strings
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+print(f"{'\\n'} symbol is a newline character.")
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+
