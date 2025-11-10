b3ab6a992e69f41a9d7d4519891a177fcf0b9663
Masoud <masoudmashayekh4944@gmail.com>
2025-11-10 17:17:38 +0100

    save

--- Day1_Variables/main.py
+++ Day1_Variables/main.py
@@ -13,4 +13,12 @@ print("Hello World!\nI'm here to be consistent in what I do.")
 print("Hello" + " " + "Masoud")
 
 # IndentationError: Spaces are very important in Python.
+# A SyntaxError happens when Python can’t understand your code because it breaks those rules.
 
+
+# The input() function in Python is used to take input from the user via the console. 
+# Whatever the user types is returned as a string.
+print("Hello " + input("What is your name?") + "!")
+
+
+# In Python, the # symbol is used to write comments.
