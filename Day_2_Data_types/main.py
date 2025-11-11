075cb86b1a450475212f00ab7260aad5c2f2ff90
Masoud <masoudmashayekh4944@gmail.com>
2025-11-11 17:01:20 +0100

    save

--- Day_2_Data_types/main.py
+++ Day_2_Data_types/main.py
@@ -3,3 +3,13 @@
 print("Hello"[0]) # Subscripting the string to get 'H'
 print("Hello"[4]) # Subscripting the string to get 'o'
 print("Hello"[-1]) # Subscripting the string to get 'o'
+
+# "123" is a string
+print("123" + "456")
+
+
+# Integer = Whole number
+print(123 + 456)
+
+# Large integer : The underscores (_) make large numbers easier to read in Python.
+print(123_345_546_333)
