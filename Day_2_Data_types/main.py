0d8569f52c230fa7b7737f7ad7c527ad3103ae40
Masoud <masoudmashayekh4944@gmail.com>
2025-11-11 16:57:10 +0100

    savw

---
+++ Day_2_Data_types/main.py
@@ -0,0 +1,5 @@
+# Strings, integer, float, boolean
+# Strings   "Hello"
+print("Hello"[0]) # Subscripting the string to get 'H'
+print("Hello"[4]) # Subscripting the string to get 'o'
+print("Hello"[-1]) # Subscripting the string to get 'o'
