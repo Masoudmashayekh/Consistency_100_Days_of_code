f987cec5e0346e00e9bce705fa24fb80783d90a9
Masoud <masoudmashayekh4944@gmail.com>
2025-11-11 17:16:38 +0100

    save

--- Day_2_Data_types/main.py
+++ Day_2_Data_types/main.py
@@ -4,21 +4,30 @@ print("Hello"[0]) # Subscripting the string to get 'H'
 print("Hello"[4]) # Subscripting the string to get 'o'
 print("Hello"[-1]) # Subscripting the string to get 'o'
 
-# "123" is a string
+# "123" is a string, str
 print("123" + "456")
 
 
-# Integer = Whole number
+# Integer = Whole number, int
 print(123 + 456)
 
 # Large integer : The underscores (_) make large numbers easier to read in Python.
 print(123_345_546_333)
 
-# Float = Floating-point number
+# Float = Floating-point number, float
 print(3.14159)
 
-# Boolean = True or False
+# Boolean = True or False, bool
 print(True)
 print(False)
+
+# type checking
+print(type(123)) # <class 'int'>
+print(type(3.14)) # <class 'float'>
+print(type("Hello")) # <class 'str'>
+print(type(True)) # <class 'bool'>
+
+# Type conversion
+
 
 
