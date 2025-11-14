74da8fef4cb856a986b3de8462ebae86b63c78be
Masoud <masoudmashayekh4944@gmail.com>
2025-11-14 21:38:42 +0100

    save

--- Day_3_Conditional_sentences/main.py
+++ Day_3_Conditional_sentences/main.py
@@ -2,7 +2,16 @@
 print("Welcome!")
 height = int(input("What is your height cm? "))
 
-if height > 120:
+if height >= 120:
     print("Can Ride")
 else:
     print("Can not Ride")
+
+# Greater than >
+# Less than <
+# Greater than or equal to >=
+# Less than or equal to <=
+# equal to ==
+# not equal to !=
+# = assigns a value.
+# == compares values.
