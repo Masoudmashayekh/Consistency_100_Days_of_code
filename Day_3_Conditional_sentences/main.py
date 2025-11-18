9fbe0a0c00d38e5eefac09b389284ec9849586b0
Masoud <masoudmashayekh4944@gmail.com>
2025-11-18 13:22:28 +0100

    save

--- Day_3_Conditional_sentences/main.py
+++ Day_3_Conditional_sentences/main.py
@@ -21,3 +21,8 @@ print("Welcome!")
 # 10 % 3 = 1 remaining
 print(10 % 3)
 print(10 / 3)
+number = int(input("Enter a number: "))
+if number % 2 == 0 :
+    print("Number is even")
+else:
+    print("Number is odd")
