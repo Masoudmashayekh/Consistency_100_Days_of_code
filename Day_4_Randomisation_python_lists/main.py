23410c21bab8c6cc1eb64ea90704b611a84e0f1a
Masoud <masoudmashayekh4944@gmail.com>
2025-11-19 17:51:40 +0100

    save

--- Day_4_Randomisation_python_lists/main.py
+++ Day_4_Randomisation_python_lists/main.py
@@ -13,3 +13,10 @@ print(random_float)
 
 random_float_2 = random.uniform(1,10)
 print(random_float_2)
+
+
+random_heads_or_tails = random.randint(0,1)
+if random_heads_or_tails == 0:
+    print("Heads")
+else:
+    print("Tails")
