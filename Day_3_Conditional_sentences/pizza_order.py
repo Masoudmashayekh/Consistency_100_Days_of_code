90763fa3e3e917eb5941e932951cf1b7c6384fae
Masoud <masoudmashayekh4944@gmail.com>
2025-11-19 10:31:08 +0100

    save

--- Day_3_Conditional_sentences/pizza_order.py
+++ Day_3_Conditional_sentences/pizza_order.py
@@ -1,3 +1,9 @@
+small_pizza = 15
+medium_pizza = 20
+large_pizza = 25
+pepperoni_small = 2
+pepperoni_l_m = 3
+cheese_price = 1
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
