8ced24cc9e95aad30c2c140b8b69a99a93081b70
Masoud <masoudmashayekh4944@gmail.com>
2025-11-19 11:22:25 +0100

    save

--- Day_3_Conditional_sentences/project.py
+++ Day_3_Conditional_sentences/project.py
@@ -1,14 +1,17 @@
 print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
 left_right = input("left or right? ").capitalize()
-swim_wiat = input("swim or wait? ").capitalize()
-which_door = input("Which door? Red, Blue, Yellow: ").capitalize()
 if left_right == "Right":
     print("Game Over.")
+    quit
 else:
+    swim_wiat = input("swim or wait? ").capitalize()
     if swim_wiat == "Swim":
         print("Game Over.")
+        quit
     else:
+        which_door = input("Which door? Red, Blue, Yellow: ").capitalize()
         if which_door == "Red" or which_door == "Blue":
             print("Game Over.")
+            quit
         elif which_door == "Yellow":
             print("You Win!")
