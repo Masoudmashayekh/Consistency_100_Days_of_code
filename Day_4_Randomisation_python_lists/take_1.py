36671fabc627c002d1056564b2841b5d259e0731
Masoud <masoudmashayekh4944@gmail.com>
2025-11-20 10:46:34 +0100

    save

---
+++ Day_4_Randomisation_python_lists/take_1.py
@@ -0,0 +1,5 @@
+# Nested List
+fruits = ["Apple", "Banana", "Orange", "Grape", "Strawberry"]
+vegetables = ["Carrot", "Broccoli", "Potato", "Tomato", "Spinach"]
+dirty_dozen = [fruits,vegetables]
+print(dirty_dozen)
