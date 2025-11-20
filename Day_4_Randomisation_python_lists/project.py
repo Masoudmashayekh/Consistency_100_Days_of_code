5a9f703501c7aaa660fcccd12a26bdd84e422f88
Masoud <masoudmashayekh4944@gmail.com>
2025-11-20 11:10:46 +0100

    save

--- Day_4_Randomisation_python_lists/project.py
+++ Day_4_Randomisation_python_lists/project.py
@@ -1,18 +1,20 @@
 import random
 list= ["Rock", "Paper", "Scissors"]
-list_len = len(list)
-pc_score = 0
-my_score = 0
 pc_chose = random.choice(list)
 my_chose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-
-if my_chose == list[0] and pc_chose == list[0]:
+print(f"Pc chose: {pc_chose}")
+print(f"My chose: {list[my_chose]}")
+if list[my_chose] == pc_chose:
     print("It's a draw.")
-elif my_chose == list[0] and pc_chose == list[2]:
+elif list[my_chose] == list[0] and pc_chose == list[2]:
     print("You win!")
-elif my_chose == list[2] and pc_chose == list[1]:
+elif list[my_chose] == list[1] and pc_chose == list[0]:
     print("You win!")
-elif my_chose == list[0] and pc_chose == list[1]:
+elif list[my_chose] == list[2] and pc_chose == list[1]:
+    print("You win!")
+elif list[my_chose] == list[0] and pc_chose == list[1]:
     print("You Lose!")
-elif my_chose == list[1] and pc_chose == list[2]:
+elif list[my_chose] == list[1] and pc_chose == list[2]:
+    print("You Lose!")
+elif list[my_chose] == list[2] and pc_chose == list[0]:
     print("You Lose!")
