74308e07d67b3d062b473138423512b87f00171e
Masoud <masoudmashayekh4944@gmail.com>
2025-11-20 10:05:49 +0100

    save

--- Day_4_Randomisation_python_lists/take.py
+++ Day_4_Randomisation_python_lists/take.py
@@ -1,6 +1,10 @@
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 
 import random
+# Option 1
 friends_len = len(friends)
 random_friends = random.randint(0, friends_len - 1)
 print(friends[random_friends])
+
+# Option 2
+print(random.choice(friends))
