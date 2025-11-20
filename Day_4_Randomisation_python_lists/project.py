09c273335887d3b0fb1f3754751df5c6b6eef35e
Masoud <masoudmashayekh4944@gmail.com>
2025-11-20 11:15:00 +0100

    save

--- Day_4_Randomisation_python_lists/project.py
+++ Day_4_Randomisation_python_lists/project.py
@@ -1,20 +1,20 @@
 import random
 list= ["Rock", "Paper", "Scissors"]
-pc_chose = random.choice(list)
-my_chose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-print(f"Pc chose: {pc_chose}")
-print(f"My chose: {list[my_chose]}")
-if list[my_chose] == pc_chose:
+pc_choice = random.choice(list)
+my_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+print(f"Pc chose: {pc_choice}")
+print(f"My chose: {list[my_choice]}")
+if list[my_choice] == pc_choice:
     print("It's a draw.")
-elif list[my_chose] == list[0] and pc_chose == list[2]:
+elif list[my_choice] == list[0] and pc_choice == list[2]:
     print("You win!")
-elif list[my_chose] == list[1] and pc_chose == list[0]:
+elif list[my_choice] == list[1] and pc_choice == list[0]:
     print("You win!")
-elif list[my_chose] == list[2] and pc_chose == list[1]:
+elif list[my_choice] == list[2] and pc_choice == list[1]:
     print("You win!")
-elif list[my_chose] == list[0] and pc_chose == list[1]:
+elif list[my_choice] == list[0] and pc_choice == list[1]:
     print("You Lose!")
-elif list[my_chose] == list[1] and pc_chose == list[2]:
+elif list[my_choice] == list[1] and pc_choice == list[2]:
     print("You Lose!")
-elif list[my_chose] == list[2] and pc_chose == list[0]:
+elif list[my_choice] == list[2] and pc_choice == list[0]:
     print("You Lose!")
