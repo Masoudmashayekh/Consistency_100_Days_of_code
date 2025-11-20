3d87f096e8b6d1eeaa187ba22d879f6bee20eff0
Masoud <masoudmashayekh4944@gmail.com>
2025-11-20 11:01:16 +0100

    save

--- Day_4_Randomisation_python_lists/project.py
+++ Day_4_Randomisation_python_lists/project.py
@@ -4,5 +4,15 @@ list_len = len(list)
 pc_score = 0
 my_score = 0
 pc_chose = random.choice(list)
+my_chose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 
-
+if my_chose == list[0] and pc_chose == list[0]:
+    print("It's a draw.")
+elif my_chose == list[0] and pc_chose == list[2]:
+    print("You win!")
+elif my_chose == list[2] and pc_chose == list[1]:
+    print("You win!")
+elif my_chose == list[0] and pc_chose == list[1]:
+    print("You Lose!")
+elif my_chose == list[1] and pc_chose == list[2]:
+    print("You Lose!")
