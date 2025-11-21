d876a7f71b455923bda58bb91260877595828a06
Masoud <masoudmashayekh4944@gmail.com>
2025-11-21 10:53:40 +0100

    save

---
+++ Day_5_for_loops_range_code_blocks/take_1.py
@@ -0,0 +1,14 @@
+student_scores = [150, 142, 120, 160, 120, 40, 54, 180, 94, 130, 200]
+
+sum_scores = 0
+for score in student_scores:
+    sum_scores += score
+    
+print(sum_scores)
+max = 0
+for score in student_scores:
+    if max <= score:
+        max = score
+   
+    
+print(max)
