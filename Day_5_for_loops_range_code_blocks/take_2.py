760dfd229e9512c7e21aa5387b61441cccde691a
Masoud <masoudmashayekh4944@gmail.com>
2025-11-21 11:04:38 +0100

    save

---
+++ Day_5_for_loops_range_code_blocks/take_2.py
@@ -0,0 +1,14 @@
+# Range Function with For Loop
+
+for number in range(1,10):
+    print(number)
+    
+for number in range(1, 10, 3):
+    print(number)
+    
+add_numbers = 0
+for number in range(1,101):
+    add_numbers += number
+    
+    
+print(add_numbers)
