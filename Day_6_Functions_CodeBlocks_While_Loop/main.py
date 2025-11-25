ea93946a01af53c574b26559518352be2e198ad9
Masoud <masoudmashayekh4944@gmail.com>
2025-11-25 10:46:39 +0100

    save

--- Day_6_Functions_CodeBlocks_While_Loop/main.py
+++ Day_6_Functions_CodeBlocks_While_Loop/main.py
@@ -6,3 +6,24 @@ def my_function():
 # calling Function
 my_function()
 
+
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def turn():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for step in range(6):
+#     turn()
+    
