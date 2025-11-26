4ad37834a81f119276cc815ee3d8ace66ce82877
Masoud <masoudmashayekh4944@gmail.com>
2025-11-26 10:32:31 +0100

    save

--- Day_6_Functions_CodeBlocks_While_Loop/project.py
+++ Day_6_Functions_CodeBlocks_While_Loop/project.py
@@ -15,6 +15,6 @@
 #     elif not right_is_clear():
 #         turn_left()
 
-
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=problem_world.json&url=user_world%3Aproblem_world.json
 
     
