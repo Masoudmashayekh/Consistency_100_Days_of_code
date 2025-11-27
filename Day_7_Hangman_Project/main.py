8f18ef4d361b23ad6b305ad86c34430b0ce09f82
Masoud <masoudmashayekh4944@gmail.com>
2025-11-27 10:18:38 +0100

    save

--- Day_7_Hangman_Project/main.py
+++ Day_7_Hangman_Project/main.py
@@ -1,2 +1,13 @@
 # Review: For & While Loops, If/else, Lists, Strings, Range, Modules
+word_list =["aardvark", "baboon", "camel"]
+
+# TODO-1 - Randomly choose a word from the word_list 
+
+
+
+# TODO-2 - Ask the user to guess a letter 
+
+
+
+# TODO-3 - Check if the letter the user guessed is right or wrong.
 
