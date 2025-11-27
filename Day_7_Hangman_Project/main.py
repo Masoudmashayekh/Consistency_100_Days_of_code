f8402d60c3869dfaa1e489c7be99c5e65c0b36e3
Masoud <masoudmashayekh4944@gmail.com>
2025-11-27 10:27:39 +0100

    save

--- Day_7_Hangman_Project/main.py
+++ Day_7_Hangman_Project/main.py
@@ -3,9 +3,9 @@ import random
 word_list =["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list 
-random_word = random.choice(word_list)
-letter_random_word = list(random_word)
-print(random_word)
+chosen_word = random.choice(word_list)
+letter_random_word = list(chosen_word)
+print(chosen_word)
 print(letter_random_word)
 
 # TODO-2 - Ask the user to guess a letter 
