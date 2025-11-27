489a88334d581e1080d6b2aceaa594f5761fc584
Masoud <masoudmashayekh4944@gmail.com>
2025-11-27 10:50:51 +0100

    save

--- Day_7_Hangman_Project/main.py
+++ Day_7_Hangman_Project/main.py
@@ -3,16 +3,20 @@ word_list =["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list 
 chosen_word = random.choice(word_list)
-letter_chosen_word = list(chosen_word)
+list_chosen_word = list(chosen_word)
 print(chosen_word)
-print(letter_chosen_word)
+print(list_chosen_word)
+num_chosen_word = list(len(chosen_word)*"_")
 
 # TODO-2 - Ask the user to guess a letter 
+print(num_chosen_word)
 guess = input("Guess a letter: ").lower()
 
 # TODO-3 - Check if the letter the user guessed is right or wrong.
-for letter in letter_chosen_word:
+for letter in list_chosen_word:
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        for letter in list_chosen_word:
+            n = list_chosen_word.index(letter)
+            num_chosen_word[n] = letter
+
+print("".join(num_chosen_word))
