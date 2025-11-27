c12753690b91378453f51144fe8c37f39222b51c
Masoud <masoudmashayekh4944@gmail.com>
2025-11-27 10:22:29 +0100

    save

--- Day_7_Hangman_Project/main.py
+++ Day_7_Hangman_Project/main.py
@@ -1,9 +1,12 @@
 # Review: For & While Loops, If/else, Lists, Strings, Range, Modules
+import random
 word_list =["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list 
-
-
+random_word = random.choice(word_list)
+letter_random_word = list(random_word)
+print(random_word)
+print(letter_random_word)
 
 # TODO-2 - Ask the user to guess a letter 
 
