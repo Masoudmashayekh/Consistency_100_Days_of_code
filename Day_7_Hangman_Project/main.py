66b7444ebd59ae66d694f18024833716e2157164
Masoud <masoudmashayekh4944@gmail.com>
2025-12-01 16:46:16 +0100

    save

--- Day_7_Hangman_Project/main.py
+++ Day_7_Hangman_Project/main.py
@@ -1,25 +1,29 @@
 import random # Review: For & While Loops, If/else, Lists, Strings, Range, Modules
 from hangman_words import word_list
-from hangman_art import HANGMANPICS
+from hangman_art import HANGMANPICS, logo
 
 # TODO-1 - Randomly choose a word from the word_list 
 chosen_word = random.choice(word_list)
 list_chosen_word = list(chosen_word)
-print(chosen_word)
-print(list_chosen_word)
+# print(chosen_word)
+# print(list_chosen_word)
 num_chosen_word = list(len(chosen_word)*"_")
 
 # TODO-2 - Ask the user to guess a letter 
-print(num_chosen_word)
+# print(num_chosen_word)
 
-
+print(logo)
 lives = 6
 hangman = 0
 game_over = False
 while not game_over :
-    print(f"*** {lives}/6 Lives left ***")
-    guess = input("Guess a letter: ").lower()
     
+    final_word = "".join(num_chosen_word)
+    print(f"Word to guess: {final_word}")
+    print(f"****** {lives}/6 Lives left ******")
+    guess = input("Guess a letter: ").lower()
+    if guess in num_chosen_word:
+        print(f"You've already guessed {guess}")
     # TODO-3 - Check if the letter the user guessed is right or wrong.
     for i in range(len(chosen_word)):
         if chosen_word[i] == guess:
