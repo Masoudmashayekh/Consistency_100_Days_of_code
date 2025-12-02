78ad35eca00e67b49a247c31f15064388a2d36b2
Masoud <masoudmashayekh4944@gmail.com>
2025-12-02 11:39:43 +0100

    save

--- Day_8_Functions_with_inputs/main.py
+++ Day_8_Functions_with_inputs/main.py
@@ -2,3 +2,20 @@
 # Functions with inputs
 # Arguments and Parameters
 
+# Type 'encode' to encrypt, type 'decode to decrypt:
+# encode
+# Type your message:
+# Hello World!
+# Type the shift number:
+# 9
+# Here's the encoded result: .....!
+# Type 'yes if you want to go again. Otherwise type 'no'.
+# Type 'encode' to encrypt, type 'decode to decrypt:
+# decode
+# Type your message:
+# adsf asdf sadf
+# Type the shift number:
+# 9
+# Here's the encoded result: .....!
+
+
