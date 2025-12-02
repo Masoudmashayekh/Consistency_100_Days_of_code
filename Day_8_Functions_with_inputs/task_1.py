631a926d94dfd239897930a94a179ca31465f2c1
Masoud <masoudmashayekh4944@gmail.com>
2025-12-02 11:01:06 +0100

    save

--- Day_8_Functions_with_inputs/task_1.py
+++ Day_8_Functions_with_inputs/task_1.py
@@ -24,3 +24,7 @@
 # Function with more than 1  input
 
 def greet_with(name,location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+    
+greet_with("Masoud", "London")
