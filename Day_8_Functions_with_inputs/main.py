d85a396e5c04e4dd39e6e311ab65a2bd291e7780
Masoud <masoudmashayekh4944@gmail.com>
2025-12-04 16:22:56 +0100

    save

--- Day_8_Functions_with_inputs/main.py
+++ Day_8_Functions_with_inputs/main.py
@@ -20,6 +20,7 @@
 import string
 
 alphabet = list(string.ascii_lowercase)
+print(alphabet)
 
 # direction = input("Type 'encode' to encrypt, type 'decode to decrypt:\n").lower()
 # text = input("Type your message:\n").lower()
