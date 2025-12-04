e2d8594cb8a2ff79c0772b574b932fbff88cf8bf
Masoud <masoudmashayekh4944@gmail.com>
2025-12-04 16:21:53 +0100

    save

--- Day_8_Functions_with_inputs/main.py
+++ Day_8_Functions_with_inputs/main.py
@@ -21,7 +21,7 @@ import string
 
 alphabet = list(string.ascii_lowercase)
 
-direction = input("Type 'encode' to encrypt, type 'decode to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# direction = input("Type 'encode' to encrypt, type 'decode to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
 
