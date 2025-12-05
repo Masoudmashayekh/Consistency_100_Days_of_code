53a3f1c33e3355c30e1b4a493c2f57eb04e2303b
Masoud <masoudmashayekh4944@gmail.com>
2025-12-05 11:50:31 +0100

    save

--- Day_9_Dictionaries_and_Nesting/task_3.py
+++ Day_9_Dictionaries_and_Nesting/task_3.py
@@ -13,3 +13,14 @@ print(travel_log["France"][1])
 
 nested_list = ["A", "B", ["C", "D"]]
 print(nested_list[2][1])
+
+travel_log_dic ={
+    "France": {
+        "num_times_visited": 8,
+        "cities_visited": ["Paris", "Lille", "Dijon"]
+    },
+    "Germany": {
+        "num_times_visited": 2,
+        "cities_visited": ["Stuttgart", "Berlin"]
+    }
+}
