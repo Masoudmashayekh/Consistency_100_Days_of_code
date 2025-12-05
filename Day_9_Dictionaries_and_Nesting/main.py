b7c4ae9dab9a2bbab04d31c5813e6109a6da0624
Masoud <masoudmashayekh4944@gmail.com>
2025-12-05 14:05:07 +0100

    save

--- Day_9_Dictionaries_and_Nesting/main.py
+++ Day_9_Dictionaries_and_Nesting/main.py
@@ -7,3 +7,18 @@
 # The winner is {name} with a bid of $121.
 # {"Code": "program instructions"}
 # {Key: Value}
+
+all_bids = {}
+print("Welcome to the secret auction program.")
+repeat = True
+while repeat:
+    your_name = input("What is your name? ")
+    your_bid = input("What is your bid? $")
+    all_bids[your_name] = your_bid 
+    repeat_q = input("Are there any other bidders? Type 'yes' or 'no'").lower()
+    if repeat_q == "no":
+        repeat = False
+
+
+
+ 
