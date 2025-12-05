d2ec75476519f6244f989c81d98d65021cf0be70
Masoud <masoudmashayekh4944@gmail.com>
2025-12-05 11:12:33 +0100

    save

--- Day_9_Dictionaries_and_Nesting/main.py
+++ Day_9_Dictionaries_and_Nesting/main.py
@@ -5,3 +5,5 @@
 # What's your bid?:
 # Are there any other bidders? Type 'yes' or 'no'.
 # The winner is {name} with a bid of $121.
+# {"Code": "program instructions"}
+# {Key: Value}
---
+++ Day_9_Dictionaries_and_Nesting/task_1.py
@@ -0,0 +1,7 @@
+my_dict = {
+    "name": "Alice",
+    "age": 30,
+    "city": "Paris"
+}
+
+
