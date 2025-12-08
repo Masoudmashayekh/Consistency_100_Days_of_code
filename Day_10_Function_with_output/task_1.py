878c4ae3c5c64cec4d8d3c21820f3ba4d3419797
Masoud <masoudmashayekh4944@gmail.com>
2025-12-08 18:29:02 +0100

    save

---
+++ Day_10_Function_with_output/task_1.py
@@ -0,0 +1,8 @@
+def format_name(f_name, l_name):
+    formated_f = f_name.title()
+    formated_l = l_name.title()
+    return f"{formated_f} + {formated_l}"
+
+
+formated_string = format_name(f_name="masoud", l_name="mashayekh")
+print(formated_string)
