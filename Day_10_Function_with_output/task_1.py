6646859dcef4952fb36565228f229e0a6a637962
Masoud <masoudmashayekh4944@gmail.com>
2025-12-13 17:08:04 +0100

    save

--- Day_10_Function_with_output/task_1.py
+++ Day_10_Function_with_output/task_1.py
@@ -1,7 +1,6 @@
 def format_name(f_name, l_name):
     if f_name == "" or l_name == "":
         return "You did not provide valid input."
-    
     formated_f = f_name.title()
     formated_l = l_name.title()
     return f"{formated_f} {formated_l}"
