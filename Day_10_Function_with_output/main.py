43093bc804f471527b3df4900ee451242651666f
Masoud <masoudmashayekh4944@gmail.com>
2025-12-13 17:54:09 +0100

    save

--- Day_10_Function_with_output/main.py
+++ Day_10_Function_with_output/main.py
@@ -4,3 +4,20 @@
 # def my_function():
 #   return 3 * 2
 # Docstrings => First line after function ''' Text '''
+
+# what is your first number? 
+# +
+# -
+# *
+# /
+# Pick an operation: 
+# What is the next number?
+# 
+# Type 'y' to continue calculating with 15 or type 'n' to start new calculation: y
+# +
+# -
+# *
+# /
+# Pick an operation:
+# What is the next number?
+# 
