cf6a85d095f1330ac363cdc83fefca6c7fa339e9
Masoud <masoudmashayekh4944@gmail.com>
2025-12-13 18:06:08 +0100

    save

--- Day_10_Function_with_output/main.py
+++ Day_10_Function_with_output/main.py
@@ -20,4 +20,13 @@
 # /
 # Pick an operation:
 # What is the next number?
-# 
+
+
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
