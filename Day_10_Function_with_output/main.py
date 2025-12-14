cbccc8646501250d3b0b9c80f2cfacbe991f1c79
Masoud <masoudmashayekh4944@gmail.com>
2025-12-14 19:45:56 +0100

    save

--- Day_10_Function_with_output/main.py
+++ Day_10_Function_with_output/main.py
@@ -46,6 +46,6 @@ continue_ = True
 while continue_:
     pick_operation = input(f"+\n-\n*\n/\nPick an operation: ")
     second_number = float(input("What is the next number?"))
-    final_result = operations[pick_operation](first_number, second_number)
+    final_result = operations[pick_operation](first_number, second_number))
     print(final_result)
     continue_or_not = input("Type 'y' to continue calculating with 15 or type 'n' to start new calculation: ")
