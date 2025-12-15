495e8f3c48d39766cca07c43829f534281f7c915
Masoud <masoudmashayekh4944@gmail.com>
2025-12-15 16:57:23 +0100

    save

--- Day_10_Function_with_output/main.py
+++ Day_10_Function_with_output/main.py
@@ -40,16 +40,22 @@ def divide(n1, n2):
 
 operations = { "+" : add, "-" : subtract, "*" : multiply, "/" : divide}
 
-should_accumulate = True
-num1 = float(input("what is your first number? "))
-while should_accumulate:
-    for symbol in operations:
-        print(symbol)
-    operation_symbol = input("Pick an operation: ")
-    num2 = float(input("What is the next number? "))
-    answer = operations[operation_symbol](num1, num2)
-
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-    choice = input(f"Type 'y' to continue calculating with {answer} or type 'n' to start new calculation: ")
-    if choice == "y":
-        num1 == answer
+def calculator():
+    should_accumulate = True
+    num1 = float(input("what is your first number? "))
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number? "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        choice = input(f"Type 'y' to continue calculating with {answer} or type 'n' to start new calculation: ")
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            calculator()
+            
+            
+calculator()
