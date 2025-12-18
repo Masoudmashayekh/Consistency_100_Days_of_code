60c39883d17b72a34116d62f9ad5499b141e7bf0
Masoud <masoudmashayekh4944@gmail.com>
2025-12-18 15:35:15 +0100

    save

--- Day_11_Blackjack_Capstone_Project/main.py
+++ Day_11_Blackjack_Capstone_Project/main.py
@@ -1,6 +1,24 @@
 # Day_11 Blackjack Capstone Project
 import random
 
+
+def test(my_cards,pc_cards):
+    my_total = sum(my_cards)
+    pc_total = sum(pc_cards)
+    print(my_cards)
+    print(pc_cards)
+    if 21 >= my_total > pc_total:
+        print("You win!")
+    elif my_total < pc_total or my_total > 21:
+        print("You loss!!!")
+    else:
+        print("Draw!")
+
+
+
+
+
+
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
 my_cards = []
@@ -14,24 +32,6 @@ print(f"Computer's first card: {pc_cards[0]}")
 decision = input("Type 'y' to get another card, type 'n' to pass:  ")
 if decision == 'y':
     my_cards.append(cards[random.randint(0, len(cards)-1)])
-    my_total = sum(my_cards)
-    pc_total = sum(pc_cards)
-    print(my_cards)
-    print(pc_cards)
-    if my_total > pc_total:
-        print("You win!")
-    elif my_total < pc_total:
-        print("You loss!!!")
-    else:
-        print("Draw!")
+    test(my_cards, pc_cards)
 else:
-    my_total = sum(my_cards)
-    pc_total = sum(pc_cards)
-    print(my_cards)
-    print(pc_cards)
-    if my_total > pc_total:
-        print("You win!")
-    elif my_total < pc_total:
-        print("You loss!!!")
-    else:
-        print("Draw!")
+    test(my_cards, pc_cards)
