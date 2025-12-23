e345bd5f37d429004438942552077e8e034de1f3
Masoud <masoudmashayekh4944@gmail.com>
2025-12-23 08:57:15 +0100

    SAVE

--- Day_12_Number_Guessing_Game/task_2.py
+++ Day_12_Number_Guessing_Game/task_2.py
@@ -3,3 +3,14 @@
 game_level = 3
 enemies = ["Skeleton", "Zombie", "Alien"]
 
+if game_level < 5:
+    new_enemy_1 = enemies[0] # new_enemy is global scope
+     
+print(new_enemy_1)
+
+
+def create_enemy():
+    if game_level < 5:
+        new_enemy_2 = enemies[1] # new_enemy is Local scope
+        
+# print(new_enemy_2) NameError: name 'new_enemy_2' is not defined
