8154f5bc9ca76a62e264a4bb577c3088d85d25dc
Masoud <masoudmashayekh4944@gmail.com>
2025-12-23 11:14:38 +0100

    save

--- Day_12_Number_Guessing_Game/main.py
+++ Day_12_Number_Guessing_Game/main.py
@@ -4,6 +4,7 @@ EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
 def check_answer(user_guess, actual_answer, turn):
+    global turns
     if user_guess > actual_answer:
         print("Too high.")
         turn -= 1
@@ -22,6 +23,14 @@ def check_answer(user_guess, actual_answer, turn):
         print(f"You got it! The answer was {actual_answer}")
         turn = 0
 
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ").capitalize()
+    if level == "Easy":
+        return EASY_LEVEL_TURNS
+    elif level == "Hard":
+        return HARD_LEVEL_TURNS
+    else:
+        return "You type wrong input!!!"
 
 
 play_again = True
@@ -31,18 +40,13 @@ while play_again:
     print("I'm thinking of a number between 1 and 100.")
     actual_answer = random.randint(1,100)
     print(actual_answer)
+ 
+ 
 
-    level = input("Choose a difficulty. Type 'easy' or 'hard': ").capitalize()
-    if level == "Easy":
-        turns = EASY_LEVEL_TURNS
-    elif level == "Hard":
-        turns = HARD_LEVEL_TURNS
-    else:
-        print("You type wrong input!!!")
-    while i > 0:    
-        print(f"You have {i} attempts remaining to guess the number.")
+    while turns > 0:    
+        print(f"You have {turns} attempts remaining to guess the number.")
         user_guess = int(input("Make a guess: "))
-        check_answer(user_guess= user_guess, actual_answer= actual_answer, turn= i)
+        check_answer(user_guess= user_guess, actual_answer= actual_answer, turn= turns)
     play = input("Do you want to play again? Type 'y' or 'n'. ").capitalize()
     if play == "N":
         play_again = False
