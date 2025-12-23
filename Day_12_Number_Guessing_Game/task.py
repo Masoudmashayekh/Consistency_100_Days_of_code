94dc598ac320881644e280b6487e7006e5754f65
Masoud <masoudmashayekh4944@gmail.com>
2025-12-23 08:47:04 +0100

    save

--- Day_12_Number_Guessing_Game/task.py
+++ Day_12_Number_Guessing_Game/task.py
@@ -14,6 +14,19 @@ print(f"enemies outside function {enemies}")
 
 
 # Local scope
-
+def drink_potion():
+    potion_strength = 2
+    print( potion_strength)
+    
+drink_potion()
+#print(potion_strength)   NameError: name 'potion_strength' is not defined
 
 # Global scope
+player_health = 10
+
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+    
+    
+drink_potion()
