2e4abb86dd22b7a4f76c67e798f50e0294ec9a83
Masoud <masoudmashayekh4944@gmail.com>
2025-12-23 11:07:37 +0100

    save

--- Day_12_Number_Guessing_Game/main.py
+++ Day_12_Number_Guessing_Game/main.py
@@ -1,40 +1,48 @@
 import random
 from art import logo
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+def check_answer(user_guess, actual_answer, turn):
+    if user_guess > actual_answer:
+        print("Too high.")
+        turn -= 1
+        if turn == 0:
+            print("You've run out of guesses, you lose.")
+        else:
+            print("Guess again.")
+    elif user_guess < actual_answer:
+        print("Too low.")
+        turn -= 1
+        if turn == 0:
+            print("You've run out of guesses, you lose.")
+        else:
+            print("Guess again.")  
+    else:
+        print(f"You got it! The answer was {actual_answer}")
+        turn = 0
+
+
+
 play_again = True
 print(logo)
 print("Welcome to Number Guessing Game!")
 while play_again:
     print("I'm thinking of a number between 1 and 100.")
-    ANSWER_NUMBER = random.randint(1,100)
-    print(ANSWER_NUMBER)
+    actual_answer = random.randint(1,100)
+    print(actual_answer)
 
-    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").capitalize()
-    if difficulty == "Easy":
-        i = 10
-    elif difficulty == "Hard":
-        i = 5
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ").capitalize()
+    if level == "Easy":
+        turns = EASY_LEVEL_TURNS
+    elif level == "Hard":
+        turns = HARD_LEVEL_TURNS
     else:
         print("You type wrong input!!!")
     while i > 0:    
         print(f"You have {i} attempts remaining to guess the number.")
-        guess_num = int(input("Make a guess: "))
-        if guess_num > ANSWER_NUMBER:
-            print("Too high.")
-            i -= 1
-            if i == 0:
-                print("You've run out of guesses, you lose.")
-            else:
-                print("Guess again.")
-        elif guess_num < ANSWER_NUMBER:
-            print("Too low.")
-            i -= 1
-            if i == 0:
-                print("You've run out of guesses, you lose.")
-            else:
-                print("Guess again.")  
-        else:
-            print("You win!")
-            i = 0
+        user_guess = int(input("Make a guess: "))
+        check_answer(user_guess= user_guess, actual_answer= actual_answer, turn= i)
     play = input("Do you want to play again? Type 'y' or 'n'. ").capitalize()
     if play == "N":
         play_again = False
