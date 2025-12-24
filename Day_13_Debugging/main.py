92e48a9c662211dc1cbaff926e2c85d11dfd4961
Masoud <masoudmashayekh4944@gmail.com>
2025-12-24 19:09:15 +0100

    save

--- Day_13_Debugging/main.py
+++ Day_13_Debugging/main.py
@@ -26,7 +26,7 @@ print(dice_images[dice_num])
 # 3. Play Computer
 year = int(input("What's your year of birth? "))
 
-if year > 1980 and year < 1994:  # should use year <= 1994 instead of year < 1994
+if year > 1980 and year <= 1994:  # should use year <= 1994 instead of year < 1994
     print("You are a millennial.")
 elif year > 1994:
     print("You are a Gen Z.")
