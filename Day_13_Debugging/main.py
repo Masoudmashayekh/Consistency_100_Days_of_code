35aa36eb17169e97277a7534e5cdec1831d4c3d1
Masoud <masoudmashayekh4944@gmail.com>
2025-12-24 19:07:04 +0100

    save

--- Day_13_Debugging/main.py
+++ Day_13_Debugging/main.py
@@ -21,3 +21,12 @@ from random import randint
 dice_images = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
 dice_num = randint(0, 5) # range should be (0, 5) not (1, 6)
 print(dice_images[dice_num])
+
+
+# 3. Play Computer
+year = int(input("What's your year of birth? "))
+
+if year > 1980 and year < 1994:
+    print("You are a millennial.")
+elif year > 1994:
+    print("You are a Gen Z.")
