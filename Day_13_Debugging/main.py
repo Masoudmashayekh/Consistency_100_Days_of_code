83cf461af0a62f0e936096280b5f17f4b66965a6
Masoud <masoudmashayekh4944@gmail.com>
2025-12-24 19:11:55 +0100

    save

--- Day_13_Debugging/main.py
+++ Day_13_Debugging/main.py
@@ -30,3 +30,6 @@ if year > 1980 and year <= 1994:  # should use year <= 1994 instead of year < 19
     print("You are a millennial.")
 elif year > 1994:
     print("You are a Gen Z.")
+    
+    
+# 4. Fix the Errors
