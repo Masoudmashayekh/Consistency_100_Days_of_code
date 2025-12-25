2d64a12d087d11856d646ee93dc1ebc5b7738e18
Masoud <masoudmashayekh4944@gmail.com>
2025-12-25 17:36:16 +0100

    save

---
+++ Day_13_Debugging/task_1.py
@@ -0,0 +1,9 @@
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+        
+        
+num = odd_or_even(1992)
+print(num)
