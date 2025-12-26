8a57be63ba35f2d44df4bc356b309e9f8e395881
Masoud <masoudmashayekh4944@gmail.com>
2025-12-26 17:11:39 +0100

    save

--- Day_15_Coffee_Machine/main.py
+++ Day_15_Coffee_Machine/main.py
@@ -44,26 +44,24 @@ def coins():
     total_money = (penny * 0.01) + (nickel * 0.05) + (dime * 0.1) + (quarter * 0.25)
     return total_money
 
+def user_input():
+    correct_input = False
+    allowable_answers = ["report", "espresso", "latte", "cappuccino", "off"]
+    while not correct_input:
+        request = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        if request not in allowable_answers: 
+            print("Please enter correct input")
+        else:
+            correct_input = True
+            return request
 
-# money = 0
-# correct_input = False
-# allowable_answers = ["report", "espresso", "latte", "cappuccino", "off"]
-# while not correct_input:
-#     global user_request
-#     user_request = input("What would you like? (espresso/latte/cappuccino): ").lower()
-#     if user_request not in allowable_answers: 
-#         print("Please enter correct input")
-#     else:
-#         correct_input = True
+
+
+money = 0
+
+user_request = user_input()
         
-#     if user_request == "report":
-#         check_resources(money)
-#     elif user_request == "espresso":
-
-
-# penny = float(input("How many penny?: "))
-# nickel = float(input("How many nickel?: "))
-# dime = float(input("How many dime?: "))
-# quarter = float(input("How many quarter?: "))
-# total_money = (penny * 0.01) + (nickel * 0.05) + (dime * 0.1) + (quarter * 0.25)
-# print(total_money)
+if user_request == "report":
+    check_resources(money)
+elif user_request == "espresso":
+    pass
