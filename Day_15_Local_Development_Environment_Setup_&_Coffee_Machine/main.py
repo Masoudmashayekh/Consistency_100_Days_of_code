afc336f68499f9259d7de43e55a257e7d95cd866
Masoud <masoudmashayekh4944@gmail.com>
2025-12-26 16:05:30 +0100

    save

--- Day_15_Local_Development_Environment_Setup_&_Coffee_Machine/main.py
+++ Day_15_Local_Development_Environment_Setup_&_Coffee_Machine/main.py
@@ -5,3 +5,5 @@
 # Cappuccino 250ml Water, 24g Coffee, 100ml Milk, $3
 
 # Machine 300ml Water, 200ml Milk, 100g Coffee
+
+# 2. Coin Operated: Penny: 1 cent, Nickel: 5 cents, Dime: 10 cents, Quarter: 25 cents, 0.01, 0.05, 0.10, 0.25
