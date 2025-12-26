c6ee458cd0f323788acd4c967f4ac2412829a704
Masoud <masoudmashayekh4944@gmail.com>
2025-12-26 10:57:41 +0100

    save

--- Day_14_Higher_Lower_Game_Project/main.py
+++ Day_14_Higher_Lower_Game_Project/main.py
@@ -1,8 +1,14 @@
 from game_data import instagram_data
 from art import VS_ART_SIMPLE, HIGH_LOW_ART
+import random
 
-print("Compare A: ")
-print("Against B: ")
-print("Who has more followers? Type 'A' or 'B': ")
-print("Sorry, that's wrong. Final score: 0")
-print("You're right! Current score: 1")
+a = instagram_data[random.randint(0, len(instagram_data)-1)]
+
+
+
+
+# print("Compare A: ")
+# print("Against B: ")
+# print("Who has more followers? Type 'A' or 'B': ")
+# print("Sorry, that's wrong. Final score: 0")
+# print("You're right! Current score: 1")
