6dd5026c352fe24bc3ffe543cf28772b9153c9d1
Masoud <masoudmashayekh4944@gmail.com>
2025-12-26 11:15:31 +0100

    save

--- Day_14_Higher_Lower_Game_Project/main.py
+++ Day_14_Higher_Lower_Game_Project/main.py
@@ -2,17 +2,21 @@ from game_data import instagram_data
 from art import VS_ART_SIMPLE, HIGH_LOW_ART
 import random
 
-a = instagram_data[random.randint(0, len(instagram_data)-1)]
-b = instagram_data[random.randint(0, len(instagram_data)-1)]
-
+A = instagram_data[random.randint(0, len(instagram_data)-1)]
+B = instagram_data[random.randint(0, len(instagram_data)-1)]
+print(HIGH_LOW_ART)
 score = 0
 continue_game = True
 while continue_game:
-    print(f"Compare A: {a["name"]}, {a["description"]}, {a["country"]} ")
-    print(f"Against B: {b["name"]}, {b["description"]}, {b["country"]} ")
-    print("Who has more followers? Type 'A' or 'B': ")
+    print(f"Compare A: {A["name"]}, {A["description"]}, {A["country"]} ")
+    print(VS_ART_SIMPLE)
+    print(f"Against B: {B["name"]}, {B["description"]}, {B["country"]} ")
+    choose = input("Who has more followers? Type 'A' or 'B': ")
 
-    if a["followers_count"] > b["followers_count"]:
+    if choose == "A" and A["followers_count"] > B["followers_count"]:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    elif choose == "B" and B["followers_count"] > A["followers_count"]:
         score += 1
         print(f"You're right! Current score: {score}")
     else:
