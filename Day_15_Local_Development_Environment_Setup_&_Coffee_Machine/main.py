c72d4988afad8756920b005e65cd1dfb7b145c43
Masoud <masoudmashayekh4944@gmail.com>
2025-12-26 16:03:07 +0100

    save

---
+++ Day_15_Local_Development_Environment_Setup_&_Coffee_Machine/main.py
@@ -0,0 +1,7 @@
+# Makes 3 hot flavours
+
+# Espresso  50ml water, 18g Coffee, $1.5
+# Latte 200mk Water, 24g Coffee, 150ml Milk, $2.5
+# Cappuccino 250ml Water, 24g Coffee, 100ml Milk, $3
+
+# Machine 300ml Water, 200ml Milk, 100g Coffee
