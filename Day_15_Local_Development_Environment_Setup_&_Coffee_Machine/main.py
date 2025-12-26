023b613aa35ad51db184594c86de95bd9010d5d2
Masoud <masoudmashayekh4944@gmail.com>
2025-12-26 16:08:22 +0100

    save

--- Day_15_Local_Development_Environment_Setup_&_Coffee_Machine/main.py
+++ Day_15_Local_Development_Environment_Setup_&_Coffee_Machine/main.py
@@ -7,3 +7,15 @@
 # Machine 300ml Water, 200ml Milk, 100g Coffee
 
 # 2. Coin Operated: Penny: 1 cent, Nickel: 5 cents, Dime: 10 cents, Quarter: 25 cents, 0.01, 0.05, 0.10, 0.25
+
+# Print Report
+# Check resources sufficient?
+
+
+
+
+# What would you like? (espresso/latte/cappuccino): report
+# Water: 300 ml
+# Milk: 200 ml
+# Coffee: 100 g
+#Money: $0
