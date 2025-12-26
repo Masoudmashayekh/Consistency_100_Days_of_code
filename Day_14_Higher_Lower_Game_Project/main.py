ddd6b221d627e5e4852a062757d746958c8bbce6
Masoud <masoudmashayekh4944@gmail.com>
2025-12-26 11:08:32 +0100

    save

--- Day_14_Higher_Lower_Game_Project/main.py
+++ Day_14_Higher_Lower_Game_Project/main.py
@@ -5,12 +5,16 @@ import random
 a = instagram_data[random.randint(0, len(instagram_data)-1)]
 b = instagram_data[random.randint(0, len(instagram_data)-1)]
 
-
-print(f"Compare A: {a["name"]}, {a["description"]}, {a["country"]} ")
-print(f"Against B: {b["name"]}, {b["description"]}, {b["country"]} ")
-print("Who has more followers? Type 'A' or 'B': ")
 score = 0
-if a["followers_count"] > b["followers_count"]:
-    score += 1
-# print("Sorry, that's wrong. Final score: 0")
-# print("You're right! Current score: 1")
+continue_game = True
+while continue_game:
+    print(f"Compare A: {a["name"]}, {a["description"]}, {a["country"]} ")
+    print(f"Against B: {b["name"]}, {b["description"]}, {b["country"]} ")
+    print("Who has more followers? Type 'A' or 'B': ")
+
+    if a["followers_count"] > b["followers_count"]:
+        score += 1
+    else:
+        
+    # print("Sorry, that's wrong. Final score: 0")
+    # print("You're right! Current score: 1")
