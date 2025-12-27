3cf7d5d7c7b7f07c69c8624084059693a8fca0ee
Masoud <masoudmashayekh4944@gmail.com>
2025-12-27 23:36:45 +0100

    save

--- Day_16_Object_Oriented_Programming_OOP/task_1.py
+++ Day_16_Object_Oriented_Programming_OOP/task_1.py
@@ -5,7 +5,14 @@ from turtle import Turtle, Screen
 timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("blue4")
+timmy.speed(1)
+
+
 
 my_screen = Screen()
 my_screen.canvheight 
+
+
+timmy.forward(300)
+
 my_screen.exitonclick()
