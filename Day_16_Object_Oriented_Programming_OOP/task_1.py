a3907f4f1b76173fb33ca69a90f7bdacedd9ce17
Masoud <masoudmashayekh4944@gmail.com>
2025-12-27 23:28:58 +0100

    save

--- Day_16_Object_Oriented_Programming_OOP/task_1.py
+++ Day_16_Object_Oriented_Programming_OOP/task_1.py
@@ -1,8 +1,10 @@
-# Turtle Graphics
+# Turtle Graphics:  "https://docs.python.org/3/library/turtle.html"
 from turtle import Turtle, Screen
 
 # timmy = turtle.Turtle()
 timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("blue4")
 
 my_screen = Screen()
 my_screen.canvheight 
