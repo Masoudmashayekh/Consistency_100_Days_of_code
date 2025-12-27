794bfbd127723e71071141950eb519c8f93ee24a
Masoud <masoudmashayekh4944@gmail.com>
2025-12-28 00:01:43 +0100

    save

--- Day_16_Object_Oriented_Programming_OOP/task_2.py
+++ Day_16_Object_Oriented_Programming_OOP/task_2.py
@@ -4,6 +4,8 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
+
 table.add_column("Pokemon Name", ["Pikachu","Squirtle", "Charmander"])
 table.add_column("Type", ["Electic","Water", "Fire"])
+table.align = "l"
 print(table)
