4ccd3ed67481a10a682302d4e2d51bb54e74e2a2
Masoud <masoudmashayekh4944@gmail.com>
2025-12-27 23:03:46 +0100

    save

--- Day_16_Object_Oriented_Programming_OOP/main.py
+++ Day_16_Object_Oriented_Programming_OOP/main.py
@@ -3,10 +3,10 @@
 # Object Oriented Programming (OOP)
 # How to use OOP?
 # Waiter:
-# has:  is_holding_plate = True
+# has(attributes):  is_holding_plate = True
 #       tables_responsible = [4, 5, 6]
 #
-# does: def take_order(table, orrder):
+# does(methods): def take_order(table, orrder):
 #           # takes order to chef
 #       def take_payment(amount):
 #           # add money to restaurant
