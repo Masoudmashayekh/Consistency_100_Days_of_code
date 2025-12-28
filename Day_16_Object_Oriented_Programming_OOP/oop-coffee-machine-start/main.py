b4799b4e8d199253c38c77436d2b1c59a98fe472
Masoud <masoudmashayekh4944@gmail.com>
2025-12-28 16:06:22 +0100

    save

--- Day_16_Object_Oriented_Programming_OOP/oop-coffee-machine-start/main.py
+++ Day_16_Object_Oriented_Programming_OOP/oop-coffee-machine-start/main.py
@@ -2,4 +2,20 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+menu = Menu()
+coffeemaker = CoffeeMaker()
+moneymachine = MoneyMachine()
 
+print(menu.get_items())
+print(menu.find_drink("latte"))
+print(coffeemaker.report())
+print(coffeemaker.is_resource_sufficient)
+print(moneymachine.report())
+
+user_order = input(f"What would you like to drink? {menu.get_items()}")
+drink_name = menu.find_drink(user_order).name
+drink_cost = menu.find_drink(user_order).cost
+drink_ingredients = menu.find_drink(user_order).ingredients
+print(drink_cost)
+print(drink_ingredients)
+print(drink_name)
