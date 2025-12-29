ee44b48cfa1f68eb0ab4ff004f19d586e6d395e8
Masoud <masoudmashayekh4944@gmail.com>
2025-12-29 19:11:01 +0100

    save

--- Day_17_Creating_Classes/quiz-game-start/main.py
+++ Day_17_Creating_Classes/quiz-game-start/main.py
@@ -1,2 +1,10 @@
 from question_model import Question
 from data import question_data
+
+question_bank = []
+for item in question_data:
+    new_q = Question(item["text"], item["answer"])
+    question_bank.append(new_q)
+    
+    
+print(question_bank)
