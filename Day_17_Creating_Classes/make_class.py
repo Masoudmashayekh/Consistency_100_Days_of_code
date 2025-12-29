ad030c4e699123900a39b06771b6236cd0cfc319
Masoud <masoudmashayekh4944@gmail.com>
2025-12-29 18:30:13 +0100

    save

--- Day_17_Creating_Classes/make_class.py
+++ Day_17_Creating_Classes/make_class.py
@@ -7,16 +7,21 @@
 #       class Car:
 # camelCase, snake_case
 class User:   # Class => PascalCase
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
     
        
-user_1 = User() # Object = Class
+# user_1 = User() # Object = Class
 # Creating Attribute, an attribute is a variable that's associated with an object.
-user_1.id = "001"
-user_1.username = "Masoud"
+# user_1.id = "001"
+# user_1.username = "Masoud"
 
-print(user_1.username)
+# print(user_1.username)
 
 # Constructor or Initializing object
 # def __init__(self):
 #   Initialise attributes
+
+user_1 = User("001", "Masoud")
+print(user_1.username)
