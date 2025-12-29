8d5dbeeaf5de827d2ff3d64537d18ecb344ea998
Masoud <masoudmashayekh4944@gmail.com>
2025-12-29 18:16:01 +0100

    save

--- Day_17_Creating_Classes/main.py
+++ Day_17_Creating_Classes/main.py
@@ -5,15 +5,3 @@
 # The correct answer was: True
 # Your current score is: 1/1.
 
-# Class is Blueprint
-# Creating Class 
-# Step 1: 
-#       class Car:
-# camelCase, snake_case
-class User:   # Class => PascalCase
-    pass
-    
-    
-    
-
-user_1 = User() # Object = Class
---
+++ Day_17_Creating_Classes/make_class.py
@@ -0,0 +1,18 @@
+# Creating Class 
+# Class is Blueprint
+
+# car.speed object.Attribute
+
+# Step 1: 
+#       class Car:
+# camelCase, snake_case
+class User:   # Class => PascalCase
+    pass
+    
+       
+user_1 = User() # Object = Class
+# Creating Attribute
+user_1.id = "001"
+user_1.username = "Masoud"
+
+print(user_1.username)
