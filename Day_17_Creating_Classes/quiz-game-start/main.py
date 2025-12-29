c7bb9808626ad57282aadd0a725692a5e9000563
Masoud <masoudmashayekh4944@gmail.com>
2025-12-29 19:22:25 +0100

    save

--- Day_17_Creating_Classes/quiz-game-start/main.py
+++ Day_17_Creating_Classes/quiz-game-start/main.py
@@ -1,12 +1,16 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
     new_q = Question(question["text"], question["answer"])
     question_bank.append(new_q)
     
+# print(question_bank)
+# print(question_bank[0].text)
+# print(question_bank[0].answer)  
+ 
     
-print(question_bank)
-print(question_bank[0].text)
-print(question_bank[0].answer) 
+test = QuizBrain(question_bank)
+print(test.question_number)
---
+++ Day_17_Creating_Classes/quiz-game-start/quiz_brain.py
@@ -0,0 +1,7 @@
+# asking the questions.
+# checking if the answer was correct.
+# checking if we're the end of the quiz.
+class QuizBrain():
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
