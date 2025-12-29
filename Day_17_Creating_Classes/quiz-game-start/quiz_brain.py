5068781b5b8a41c263c6a469dc20734823cff0c4
Masoud <masoudmashayekh4944@gmail.com>
2025-12-29 19:34:14 +0100

    save

--- Day_17_Creating_Classes/quiz-game-start/quiz_brain.py
+++ Day_17_Creating_Classes/quiz-game-start/quiz_brain.py
@@ -8,8 +8,7 @@ class QuizBrain():
         self.question_list = question_list
  
     def next_question(self):
-        self.question = self.question_list[self.question_number].text
-        self.answer = self.question_list[self.question_number].answer
+        current_question = self.question_list[self.question_number]
         self.question_number += 1
-        self.user_answer = input(f"Q.{self.question_number}: {self.question} (True/False)?:")
+        self.user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?:")
         
