baca1bc51992c47505bf86d0d77d7623e0b19e81
Masoud <masoudmashayekh4944@gmail.com>
2025-12-29 18:46:15 +0100

    save

--- Day_17_Creating_Classes/make_class.py
+++ Day_17_Creating_Classes/make_class.py
@@ -8,9 +8,14 @@
 # camelCase, snake_case
 class User:   # Class => PascalCase
     def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-    
+        self.id = user_id # Attributes
+        self.username = username # Attributes
+        self.followers = 0
+        self.following = 0
+        
+    def follow(self, user):  # method
+       user.followers += 1
+       self.following += 1
        
 # user_1 = User() # Object = Class
 # Creating Attribute, an attribute is a variable that's associated with an object.
@@ -24,4 +29,11 @@ class User:   # Class => PascalCase
 #   Initialise attributes
 
 user_1 = User("001", "Masoud")
-print(user_1.username)
+user_2 = User("002", "Angela")
+# The attributes are the things that the object has and the methods are the things that the object does.
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
