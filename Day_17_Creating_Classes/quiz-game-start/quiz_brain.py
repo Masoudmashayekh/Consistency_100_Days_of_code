a489ce91119c7a19d6de13d3aa6da9ee03a9e49c
Masoud <masoudmashayekh4944@gmail.com>
2025-12-29 19:51:31 +0100

    save

--- Day_17_Creating_Classes/quiz-game-start/quiz_brain.py
+++ Day_17_Creating_Classes/quiz-game-start/quiz_brain.py
@@ -6,6 +6,10 @@ class QuizBrain():
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+            
  
     def next_question(self):
         current_question = self.question_list[self.question_number]
