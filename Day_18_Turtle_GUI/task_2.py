64ddd4fd3e3118b3b710700eb259596281714521
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 15:12:34 +0100

    save

--- Day_18_Turtle_GUI/task_2.py
+++ Day_18_Turtle_GUI/task_2.py
@@ -6,9 +6,16 @@ import turtle # Keyword & Module name
 # from turtle import *   => You can use everything that's in that module (But it is not usefull)
 # import turtle as t => keyword, module name, keyword, alias name
 timmy_the_tutle = turtle.Turtle()
-timmy_the_tutle.forward(100)
-timmy_the_tutle.pendown(40)
+timmy_the_tutle.pen(speed = 1)
 
-
+i = 50
+while i > 0:
+    timmy_the_tutle.pendown()
+    timmy_the_tutle.forward(5)
+    timmy_the_tutle.penup()    
+    timmy_the_tutle.forward(5)
+    i -= 1
+    
+    
 screen = turtle.Screen()
 screen.exitonclick()
