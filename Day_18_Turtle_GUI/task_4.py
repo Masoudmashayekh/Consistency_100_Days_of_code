eae017964cc7bed9d85532ab5d691a679c91df50
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 20:14:09 +0100

    save

--- Day_18_Turtle_GUI/task_4.py
+++ Day_18_Turtle_GUI/task_4.py
@@ -1,8 +1,14 @@
-# Task 4
+# Task 4: Draw a Random Walk
 from turtle import Turtle, Screen
+import random
+
+random_trun = random.choicep["right", "left"]
+
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.speed(1)
+timmy_the_turtle.pensize(5)
+
 
 
 
