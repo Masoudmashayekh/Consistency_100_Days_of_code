74196897d182a7f63464f98080d61e87d1a6e138
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 15:14:48 +0100

    save

--- Day_18_Turtle_GUI/task_2.py
+++ Day_18_Turtle_GUI/task_2.py
@@ -8,13 +8,13 @@ import turtle # Keyword & Module name
 timmy_the_tutle = turtle.Turtle()
 timmy_the_tutle.pen(speed = 1)
 
-i = 50
-while i > 0:
+
+for _ in range(20):
     timmy_the_tutle.pendown()
     timmy_the_tutle.forward(5)
     timmy_the_tutle.penup()    
     timmy_the_tutle.forward(5)
-    i -= 1
+    
     
     
 screen = turtle.Screen()
