406de989701b6fb83b92fb16b8149bd598360e95
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 13:54:56 +0100

    save

--- Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/task_2.py
+++ Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/task_2.py
@@ -3,7 +3,7 @@ import turtle # Keyword & Module name
 
 # from turtle import Turtle, Screen #  Keyword & Module name & Keyword & Thing in Module
 # from turtle import *   => You can use everything that's in that module (But it is not usefull)
-
+# import turtle as t => keyword, module name, keyword, alias name
 timmy_the_tutle = turtle.Turtle()
 
 
