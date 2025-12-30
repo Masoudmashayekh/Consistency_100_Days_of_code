4a164ffa083e16c85c3759d37b0ba30b861df9d0
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 15:20:08 +0100

    save

---
+++ Day_18_Turtle_GUI/task_3.py
@@ -0,0 +1,16 @@
+# Task 3
+from turtle import Turtle, Screen
+colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "black", "gray"]
+
+timmy_the_turtle = Turtle()
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
