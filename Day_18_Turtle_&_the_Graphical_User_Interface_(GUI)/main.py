73689f53c552fc8ad982603ee38ca1d1d66adcbc
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 13:36:02 +0100

    save

--- Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/main.py
+++ Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/main.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("navy")
+timmy_the_turtle.color("navy") # tk color specification string
 
 
 
