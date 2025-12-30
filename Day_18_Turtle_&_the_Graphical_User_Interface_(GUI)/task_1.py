263d17d7194b722614748cb5372accdbdd2a56ae
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 13:44:12 +0100

    save

---
+++ Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/task_1.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+for i in range(0,4):
+    timmy_the_turtle.forward(300)
+    timmy_the_turtle.right(90)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
