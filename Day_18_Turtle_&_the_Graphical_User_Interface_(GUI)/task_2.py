bf412ae9e8e15f4f50bfb2d5e98de5afbc22e4f3
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 13:53:43 +0100

    save

--- Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/task_2.py
+++ Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/task_2.py
@@ -1,13 +1,13 @@
-# import turtle # Keyword & Module name
+import turtle # Keyword & Module name
 # timmy_the_turtle = turtle.Turtle()
 
-from turtle import Turtle, Screen #  Keyword & Module name & Keyword & Thing in Module
+# from turtle import Turtle, Screen #  Keyword & Module name & Keyword & Thing in Module
 # from turtle import *   => You can use everything that's in that module (But it is not usefull)
 
-timmy_the_tutle = Turtle()
+timmy_the_tutle = turtle.Turtle()
 
 
 
 
-screen = Screen()
-screen.
+screen = turtle.Screen()
+screen.exitonclick()
