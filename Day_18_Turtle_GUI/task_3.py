382bf84b43b466bba4ec367812bf75f9504b6611
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 15:42:26 +0100

    save

--- Day_18_Turtle_GUI/task_3.py
+++ Day_18_Turtle_GUI/task_3.py
@@ -1,12 +1,21 @@
 # Task 3
 from turtle import Turtle, Screen
-colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "black", "gray"]
+import random
+colors = ["red", "green", "blue", "orange", "purple", "black", "gray"]
 draw_list = [3, 4, 5, 6, 7, 8, 9]
 
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.speed(1)
 
-
+for i in range(0, len(draw_list)):
+    for _ in range(0, draw_list[i]):
+        angle = 360/draw_list[i]
+        timmy_the_turtle.color(colors[i])
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(angle)
+ 
+    
 
 
 
