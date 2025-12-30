8d4313538bdc5bcdcab0d31df186fd28a114b837
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 15:21:38 +0100

    save

--- Day_18_Turtle_GUI/task_3.py
+++ Day_18_Turtle_GUI/task_3.py
@@ -1,6 +1,8 @@
 # Task 3
 from turtle import Turtle, Screen
 colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "black", "gray"]
+draw_list = [3, 4, 5, 6, 7, 8, 9]
+
 
 timmy_the_turtle = Turtle()
 
