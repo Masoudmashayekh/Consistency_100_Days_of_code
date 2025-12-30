28f99733cb7bc3b40bee404d705ecde11979f01a
Masoud <masoudmashayekh4944@gmail.com>
2025-12-30 13:29:30 +0100

    save

--- Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/main.py
+++ Day_18_Turtle_&_the_Graphical_User_Interface_(GUI)/main.py
@@ -1,1 +1,17 @@
 # Day 18: Turtle Graphics, Tuples and Importing Modules
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
