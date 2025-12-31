9d13cad74916cbcb1baaa80d4ef930e4c3dc0e73
Masoud <masoudmashayekh4944@gmail.com>
2025-12-31 16:36:25 +0100

    save

--- Day_18_Turtle_GUI/task_5.py
+++ Day_18_Turtle_GUI/task_5.py
@@ -1,10 +1,22 @@
-# Task 4: Make a Spirograph
+# Task 5: Make a Spirograph
 from turtle import Turtle, Screen, colormode
+import random
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.speed(0)
 colormode(255)
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
+
+for i in range(360):
+    timmy_the_turtle.left(i - (i-1))
+    timmy_the_turtle.color(random_color())
+    timmy_the_turtle.circle(100)
 
 
 
