644a2eb08fa5a249c596fd7bccca58a40a1a4ecc
Masoud <masoudmashayekh4944@gmail.com>
2025-12-31 17:28:06 +0100

    save

--- Day_18_Turtle_GUI/main.py
+++ Day_18_Turtle_GUI/main.py
@@ -2,10 +2,21 @@
 from turtle import Turtle, Screen, colormode
 import colorgram # colorgram.py is a Python library that lets you extract colors from images.
 import random
+from pathlib import Path
 
-colors = colorgram.extract("/Users/masoudmashayekh/Documents/Python/Consistency_100_Days_of_code/Day_18_Turtle_GUI/image.jpg", 5)
+BASE_DIR = Path(__file__).parent
+image_path = BASE_DIR / "./images/image.jpg"
 
-colors_list = []
+colors = colorgram.extract(image_path, 15)
+
+
+rgb_colors_list = []
 for color in colors:
-    rgb = color.rgb
-    color.append(rgb)
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgb = (r, g, b)
+    rgb_colors_list.append(rgb)
+    
+    
+print(rgb_colors_list)
