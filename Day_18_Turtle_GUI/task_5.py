1c67452786a93b0c1fac0d468e3f5c44a2686368
Masoud <masoudmashayekh4944@gmail.com>
2025-12-31 16:24:42 +0100

    save

---
+++ Day_18_Turtle_GUI/task_5.py
@@ -0,0 +1,18 @@
+# Task 4: Make a Spirograph
+from turtle import Turtle, Screen, colormode
+
+timmy_the_turtle = Turtle()
+colormode(255)
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
