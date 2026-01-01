d42010e9b968ee900e5911fb32299f3259fdea0e
Masoud <masoudmashayekh4944@gmail.com>
2026-01-01 17:15:16 +0100

    save

--- Day_19/base.py
+++ Day_19/base.py
@@ -2,6 +2,15 @@ from turtle import Turtle, Screen, colormode
 
 
 timmy = Turtle()
-
 screen = Screen()
+
+def move_forwards():
+    timmy.forward(10)
+
+
+screen.listen()
+screen.onkey(key="space", fun=move_forwards)
+
+
 screen.exitonclick()
+
