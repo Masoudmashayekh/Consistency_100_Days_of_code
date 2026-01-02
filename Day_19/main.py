a5cde2c0fc965329d7b72bcc170fad39b9f1332b
Masoud <masoudmashayekh4944@gmail.com>
2026-01-02 22:59:12 +0100

    save

--- Day_19/main.py
+++ Day_19/main.py
@@ -4,6 +4,7 @@ from turtle import Turtle, Screen, colormode
 screen = Screen()
 screen.setup(width= 500, height= 400)
 user_bet = screen.textinput(title= "Make your bet", prompt= "Which turtle will win the race? Enter a color: ").lower()
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 for i in range(5):
     timy = Turtle(shape= "turtle")
