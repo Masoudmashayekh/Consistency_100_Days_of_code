61157d54c263a9f933922a41527058df9066512c
Masoud <masoudmashayekh4944@gmail.com>
2026-01-02 23:08:28 +0100

    save

--- Day_19/main.py
+++ Day_19/main.py
@@ -6,12 +6,15 @@ screen.setup(width= 500, height= 400)
 user_bet = screen.textinput(title= "Make your bet", prompt= "Which turtle will win the race? Enter a color: ").lower()
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
+list = []
 for turtle_index in range(6):
     timy = Turtle(shape= "turtle")
     timy.color(colors[turtle_index])
     timy.penup()
     timy.goto(x= -220, y= -110 + (turtle_index * 50))
-
+    list.append(timy)
+    
+print(list)
 
 
 screen.exitonclick()
