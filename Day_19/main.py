9dbc0352669a902f47c561b2398fda7158c30632
Masoud <masoudmashayekh4944@gmail.com>
2026-01-02 22:48:22 +0100

    save

--- Day_19/main.py
+++ Day_19/main.py
@@ -5,7 +5,7 @@ timy = Turtle()
 screen = Screen()
 screen.setup(width= 500, height= 400)
 
-
+text_input = screen.textinput(title= "Make your bet", prompt= "Which turtle will win the race? Enter a color: ")
 
 
 
