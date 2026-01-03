e57cf862404db62c85eb14e426e333b6abea52b3
Masoud <masoudmashayekh4944@gmail.com>
2026-01-03 19:52:50 +0100

    save

--- Day_19/main.py
+++ Day_19/main.py
@@ -1,12 +1,14 @@
 # More Turtle Graphics, Event Listeners, State and Multiple Instances
 from turtle import Turtle, Screen, colormode
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(width= 500, height= 400)
 user_bet = screen.textinput(title= "Make your bet", prompt= "Which turtle will win the race? Enter a color: ").lower()
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-list = []
+turtles_list = []
 for turtle_index in range(6):
     timy = Turtle(shape= "turtle")
     timy.color(colors[turtle_index])
@@ -14,7 +16,14 @@ for turtle_index in range(6):
     timy.goto(x= -220, y= -110 + (turtle_index * 50))
     list.append(timy)
     
-print(list)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    random_distance = random.randint(0, 10)
+    
+    
 
 
 screen.exitonclick()
