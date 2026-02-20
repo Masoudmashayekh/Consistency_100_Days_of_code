c4b980a386a146fec0f99390d5d864e8eb2dc6fb
Masoud <masoudmashayekh4944@gmail.com>
2026-02-20 11:47:52 +0100

    save

--- Day_20_Snake_Game_Part_1/main.py
+++ Day_20_Snake_Game_Part_1/main.py
@@ -1,10 +1,10 @@
 from turtle import Screen, Turtle
-
+import time
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
@@ -16,13 +16,14 @@ for position in starting_positions:
     new_segment.goto(position)
     segments.append(new_segment)
     
-
+    
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
+    screen.update()
     for seg in segments:
         seg.forward(20)
-    
-
+        
 
  
 
