7893f48ccba2b3aec6c5a0cef7669dfe7307c461
Masoud <masoudmashayekh4944@gmail.com>
2026-02-20 11:41:00 +0100

    save

--- Day_20_Snake_Game_Part_1/main.py
+++ Day_20_Snake_Game_Part_1/main.py
@@ -6,17 +6,21 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 
 
-
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
 
-for segment in range(3):
+for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
     new_segment.penup()
-    new_segment.goto(x= 0 - segment * 20, y= 0)
-    segments.append(segment)
+    new_segment.goto(position)
+    segments.append(new_segment)
     
 
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
     
 
 
