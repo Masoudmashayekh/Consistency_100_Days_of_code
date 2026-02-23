f51e4c97101ab767c2234cef6b2042db035a2afc
Masoud <masoudmashayekh4944@gmail.com>
2026-02-23 14:40:53 +0100

    save

--- Day_20_Snake_Game_Part_1/snake.py
+++ Day_20_Snake_Game_Part_1/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
