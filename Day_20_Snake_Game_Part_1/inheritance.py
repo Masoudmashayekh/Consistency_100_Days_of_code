583ce557ee0d2009dbcade017aa52cec9986f7f6
Masoud <masoudmashayekh4944@gmail.com>
2026-02-24 15:29:08 +0100

    save

--- Day_20_Snake_Game_Part_1/inheritance.py
+++ Day_20_Snake_Game_Part_1/inheritance.py
@@ -9,10 +9,15 @@ class Animal:
 class Fish(Animal):
     def __init__(self):
         super().__init__()
-        
+    
+    def breathe(self):
+        super().breathe()
+        print("Doing this underwater.") 
     
     def swim(self):
         print("Moving in water.")
         
 nemo = Fish()
-nemo.
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
