b8306e8ad00ca40ea7e8d68513b4256a104f4f45
Masoud <masoudmashayekh4944@gmail.com>
2026-02-25 17:36:25 +0100

    save

--- Day_20_Snake_Game_Part_1/scoreboard.py
+++ Day_20_Snake_Game_Part_1/scoreboard.py
@@ -8,10 +8,14 @@ class ScoreBoard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(0, 270)  
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.update_scoreboard()
               
+        
+    def update_scoreboard(self):
+         self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))   
+        
         
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.update_scoreboard()
