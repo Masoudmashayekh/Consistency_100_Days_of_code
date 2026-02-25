f4fc35926fd466e2182f4842a23e01011eb39d21
Masoud <masoudmashayekh4944@gmail.com>
2026-02-25 17:27:23 +0100

    save

--- Day_20_Snake_Game_Part_1/main.py
+++ Day_20_Snake_Game_Part_1/main.py
@@ -15,7 +15,7 @@ def quit_program():
 
 snake = Snake()
 food = Food()
-board = ScoreBoard()
+scoreboard = ScoreBoard()
 
 
 screen.listen()
@@ -37,6 +37,7 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 
--- Day_20_Snake_Game_Part_1/scoreboard.py
+++ Day_20_Snake_Game_Part_1/scoreboard.py
@@ -3,9 +3,14 @@ from turtle import Turtle
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.color("red")
         self.score = 0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 270)  
         self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
-                   
+              
         
-            
+    def increase_score(self):
+        self.score += 1
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
