09b0935740fe9cf230c797de4bb104bebd977046
Masoud <masoudmashayekh4944@gmail.com>
2026-02-27 16:22:41 +0100

    save

--- Day_22_Pong_Game/ball.py
+++ Day_22_Pong_Game/ball.py
@@ -1,9 +1,20 @@
 from turtle import Turtle
+import random
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.color("red")
+        self.penup()
         self.turtlesize(stretch_len= 1, stretch_wid= 1)
         self.goto(0, 0)
+        
+        
+    def ball_move(self):
+        # self.heading(random(0,45))
+        if -290 < self.xcor() < 290 and -290 < self.ycor() < 290:
+            new_y = self.ycor() + 1
+            new_x = self.xcor() + 1
+            self.goto(new_x, new_y)
+            
