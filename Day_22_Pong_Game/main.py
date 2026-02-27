5b12dab94cf68c91a989c89c4370f85743c28159
Masoud <masoudmashayekh4944@gmail.com>
2026-02-27 13:41:14 +0100

    save

--- Day_22_Pong_Game/main.py
+++ Day_22_Pong_Game/main.py
@@ -1,8 +1,21 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width= 800, height= 600)
 screen.title("Pong Game")
+
+padel = Turtle()
+padel.color("red")
+padel.penup()
+# padel.hideturtle()
+padel.turtlesize(stretch_wid= 20, stretch_len= 100)
+
+ 
+
+
+
+
+
 
 screen.exitonclick()
