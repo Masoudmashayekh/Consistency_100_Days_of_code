dca2126eab0fec6cf4b7c6d6c3ae5e886f1f44b5
Masoud <masoudmashayekh4944@gmail.com>
2026-02-27 16:45:33 +0100

    save

--- Day_22_Pong_Game/ball.py
+++ Day_22_Pong_Game/ball.py
@@ -7,13 +7,15 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("blue")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
         
         
         
     def move(self):
-        # self.heading(random(0,45))
-        if -290 < self.xcor() < 290 and -290 < self.ycor() < 290:
-            new_y = self.ycor() + 1
-            new_x = self.xcor() + 1
-            self.goto(new_x, new_y)
-            
+        new_y = self.ycor() + self.y_move
+        new_x = self.xcor() + self.x_move
+        self.goto(new_x, new_y)
+        
+        
+    def boun
--- Day_22_Pong_Game/main.py
+++ Day_22_Pong_Game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+import time 
 
 screen = Screen()
 screen.bgcolor("black")
@@ -22,6 +23,7 @@ screen.onkey(l_paddel.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     ball.move()
     screen.update()
  
