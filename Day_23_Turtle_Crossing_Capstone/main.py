277e7f0ddf8e5be05c35792b23eba5e226f2eb4f
Masoud <masoudmashayekh4944@gmail.com>
2026-03-09 16:40:49 +0100

    save

--- Day_23_Turtle_Crossing_Capstone/main.py
+++ Day_23_Turtle_Crossing_Capstone/main.py
@@ -1,5 +1,8 @@
 import time
 from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width= 600, height= 600)
