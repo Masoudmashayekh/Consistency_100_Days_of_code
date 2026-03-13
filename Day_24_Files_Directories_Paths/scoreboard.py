fbee5a147185e9ce4f54fe1fb564459e2334f7d3
Masoud <masoudmashayekh4944@gmail.com>
2026-03-13 16:38:08 +0100

    save

--- Day_24_Files_Directories_Paths/scoreboard.py
+++ Day_24_Files_Directories_Paths/scoreboard.py
@@ -8,6 +8,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
