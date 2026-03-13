395167e2deb512fb54d8c03ebbf775e6cdabd766
Masoud <masoudmashayekh4944@gmail.com>
2026-03-13 16:54:33 +0100

    save

--- Day_24_Files_Directories_Paths/snake.py
+++ Day_24_Files_Directories_Paths/snake.py
@@ -29,8 +29,9 @@ class Snake:
     
     
     def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
         self.segments.clear()
-        self.
         self.create_snake()
         self.head = self.segments[0]
     
