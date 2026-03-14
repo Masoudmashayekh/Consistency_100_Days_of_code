746ccd65bba092dc60fba3380c27f5f47cfcb43b
Masoud <masoudmashayekh4944@gmail.com>
2026-03-14 23:21:11 +0100

    save

--- Day_24_Files_Directories_Paths/draft.py
+++ Day_24_Files_Directories_Paths/draft.py
@@ -2,6 +2,6 @@
 # contents = file.read()
 # print(contents)
 # file.close()
-with open("my_file.txt") as file:
+with open("/Users/masoudmashayekh/Documents/Python/Consistency_100_Days_of_code/Day_24_Files_Directories_Paths/my_file.txt") as file:
     contents = file.read()
     print(contents)
