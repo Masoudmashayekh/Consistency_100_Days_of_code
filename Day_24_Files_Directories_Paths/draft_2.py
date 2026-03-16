908bd0f67b2eb0c0bd9027c37de59181d8b41863
Masoud <masoudmashayekh4944@gmail.com>
2026-03-16 17:07:18 +0100

    save

--- Day_24_Files_Directories_Paths/draft_2.py
+++ Day_24_Files_Directories_Paths/draft_2.py
@@ -1,4 +1,4 @@
-# Absolute File Path
+# Absolute File Path: about Root and goes from Root
 # /Root                                 /
 #     |_Work                            /Work
 #           |_report.doc                /Work/report.doc
@@ -6,11 +6,10 @@
 #           |_Project                   /Work/Project
 #                    |_talk.ppt         /Work/Project/talk.ppt
 
-# Relative File Path:
+# Relative File Path: About where you are and where you want to go
 
 # If we are here:|_Work => ./Project/talk.ppt
 # main.py => ./report.doc
 
 
 # If we are here:|_Project and for go back => ../report.doc
-
