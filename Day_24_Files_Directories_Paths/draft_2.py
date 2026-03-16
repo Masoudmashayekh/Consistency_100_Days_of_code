d32691044a9f18f6bc182a6d3e8a0eb4e2277c6a
Masoud <masoudmashayekh4944@gmail.com>
2026-03-16 17:00:21 +0100

    save

--- Day_24_Files_Directories_Paths/draft_2.py
+++ Day_24_Files_Directories_Paths/draft_2.py
@@ -6,9 +6,11 @@
 #           |_Project                   /Work/Project
 #                    |_talk.ppt         /Work/Project/talk.ppt
 
-# Relative File Path
+# Relative File Path:
+
 # If we are here:|_Work => ./Project/talk.ppt
+# main.py => ./report.doc
+
 
 # If we are here:|_Project and for go back => ../report.doc
 
-# main.py => ./report.doc
