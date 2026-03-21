c9a6461073d15e52a060bfa226fc05985e4c76b3
Masoud <masoudmashayekh4944@gmail.com>
2026-03-21 23:42:57 +0100

    save

--- Day_25_CSV_Files_Pandas/Draft_1.py
+++ Day_25_CSV_Files_Pandas/Draft_1.py
@@ -9,10 +9,9 @@ import csv
 
 with open("./Day_25_CSV_Files_Pandas/weather_data.csv", mode= "r") as data_file:
     data = csv.reader(data_file)
-    data = list(data)
     temperatures = []
-    for row in data[1:]:
-        temp = row[1]
-        temperatures.append(int(temp))
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
 
     print(temperatures)
