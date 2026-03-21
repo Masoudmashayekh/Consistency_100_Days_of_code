dc358ace339da15b760a3885aecd4bd79aa44837
Masoud <masoudmashayekh4944@gmail.com>
2026-03-21 23:31:32 +0100

    save

--- Day_25_CSV_Files_Pandas/Draft_1.py
+++ Day_25_CSV_Files_Pandas/Draft_1.py
@@ -1,6 +1,6 @@
 # Day 25: Working with CSV Files and Analysing Data with Pandas 
 
 
-with open("./Day_25_CSV_Files_Pandas/weather_data.csv", mode= "r") as file:
-    contents = file.readline()
-    print(contents)
+with open("./Day_25_CSV_Files_Pandas/weather_data.csv", mode= "r") as data_file:
+    data = data_file.readlines()
+    print(data)
