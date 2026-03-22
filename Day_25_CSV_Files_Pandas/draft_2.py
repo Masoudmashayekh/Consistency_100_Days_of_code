759b85ac4154f18f9587316ac9c41ddff4d84eaf
Masoud <masoudmashayekh4944@gmail.com>
2026-03-22 19:09:52 +0100

    save

--- Day_25_CSV_Files_Pandas/draft_2.py
+++ Day_25_CSV_Files_Pandas/draft_2.py
@@ -1,7 +1,10 @@
 import pandas
 
 data = pandas.read_csv("./Day_25_CSV_Files_Pandas/weather_data.csv")
-print(data)
-print(data["temp"])
-print(type(data))
-print(type(data["temp"]))
+# print(data)
+# print(data["temp"])
+# print(type(data))
+# print(type(data["temp"]))
+
+data_dict = data.to_dict()
+print(data_dict)
