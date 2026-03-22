508508014c9c71d8de332350f111cab679951bb0
Masoud <masoudmashayekh4944@gmail.com>
2026-03-22 19:23:32 +0100

    save

--- Day_25_CSV_Files_Pandas/draft_2.py
+++ Day_25_CSV_Files_Pandas/draft_2.py
@@ -6,17 +6,20 @@ data = pandas.read_csv("./Day_25_CSV_Files_Pandas/weather_data.csv")
 # print(type(data))
 # print(type(data["temp"]))
 
-data_dict = data.to_dict()
-print(data_dict)
+# data_dict = data.to_dict()
+# print(data_dict)
 
-temp_list = data["temp"].to_list()
-print(temp_list)
-print(len(temp_list))
-temp_average = (sum(temp_list)/len(temp_list))
-print(temp_average)
-print(data["temp"].mean())
-print(data["temp"].max())
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+# print(len(temp_list))
+# temp_average = (sum(temp_list)/len(temp_list))
+# print(temp_average)
+# print(data["temp"].mean())
+# print(data["temp"].max())
 
 # Get data in columns
-print(data["condition"])
-print(data.day)
+# print(data["condition"])
+# print(data.day)
+
+# Get data in Row
+print(data[data.day == "Monday"])
