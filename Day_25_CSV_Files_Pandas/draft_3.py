79f20059301f68d4e143ea45ceb85721f942b39a
Masoud <masoudmashayekh4944@gmail.com>
2026-03-24 19:46:50 +0100

    save

--- Day_25_CSV_Files_Pandas/draft_3.py
+++ Day_25_CSV_Files_Pandas/draft_3.py
@@ -2,9 +2,21 @@ import pandas
 
 data = pandas.read_csv("./Day_25_CSV_Files_Pandas/Central_Park_Squirrel_Census_Squirrel_Data.csv")
 print(data.Primary_Fur_Color)
-x = 0
-y = 0
-z = 0
+gray = 0
+cinnamon = 0
+black = 0
 
-gray = data.Primary_Fur_Color == "Gray"
-print(gray)
+data_list = data.Primary_Fur_Color.to_list()
+for i in data_list:
+    if i == "Gray":
+        gray += 1
+    elif i == "Cinnamon":
+        cinnamon += 1
+    elif i == "Black":
+        black += 1
+       
+print(f"{gray}, {cinnamon}, {black}")
+        
+data_dict ={
+    "Fur Color":["Gray", ""]
+}
