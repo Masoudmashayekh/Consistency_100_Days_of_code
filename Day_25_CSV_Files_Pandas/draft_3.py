2137eb9cbb896a6b0585aa02e5f1002c56fc4f4c
Masoud <masoudmashayekh4944@gmail.com>
2026-03-24 19:35:55 +0100

    save

--- Day_25_CSV_Files_Pandas/draft_3.py
+++ Day_25_CSV_Files_Pandas/draft_3.py
@@ -2,3 +2,9 @@ import pandas
 
 data = pandas.read_csv("./Day_25_CSV_Files_Pandas/Central_Park_Squirrel_Census_Squirrel_Data.csv")
 print(data.Primary_Fur_Color)
+x = 0
+y = 0
+z = 0
+
+gray = data.Primary_Fur_Color == "Gray"
+print(gray)
