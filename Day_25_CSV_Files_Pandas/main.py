5933e9e24ed4d6e1522dce714ca7dff95a31fec7
Masoud <masoudmashayekh4944@gmail.com>
2026-03-25 17:22:50 +0100

    save

--- Day_25_CSV_Files_Pandas/main.py
+++ Day_25_CSV_Files_Pandas/main.py
@@ -3,8 +3,10 @@ import turtle
 
 screen = turtle.Screen()
 screen.title("U.S States Games")
+image = "./Day_25_CSV_Files_Pandas/blank_states_img.gif"
+screen.addshape(image)
 
-
+turtle.shape(image)
 
 
 screen.exitonclick()
