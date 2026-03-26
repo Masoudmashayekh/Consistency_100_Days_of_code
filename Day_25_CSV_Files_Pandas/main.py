1fab20bf98dc12332b46af587f671c8f444c348c
Masoud <masoudmashayekh4944@gmail.com>
2026-03-26 22:37:12 +0100

    save

--- Day_25_CSV_Files_Pandas/main.py
+++ Day_25_CSV_Files_Pandas/main.py
@@ -2,6 +2,8 @@ import turtle
 import pandas
 # Turtle just works with image.gif
 data = pandas.read_csv("./Day_25_CSV_Files_Pandas/50_states.csv")
+data_dict = data.to_dict
+print(data_dict)
 screen = turtle.Screen()
 screen.title("U.S States Games")
 image = "./Day_25_CSV_Files_Pandas/blank_states_img.gif"
