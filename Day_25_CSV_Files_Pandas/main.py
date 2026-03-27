1c1548a26e24bcde9a6fd5862ed7d597b2a5a551
Masoud <masoudmashayekh4944@gmail.com>
2026-03-27 23:43:36 +0100

    save

--- Day_25_CSV_Files_Pandas/main.py
+++ Day_25_CSV_Files_Pandas/main.py
@@ -21,7 +21,9 @@ if answer_state in all_states:
     t = turtle.Turtle()
     t.penup()
     t.hideturtle()
-
+    state_data = data[data.state == answer_state]
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(answer_state)
 
 
 
