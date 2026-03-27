22cb341e560403ea21be17a260e938e0933e5cf6
Masoud <masoudmashayekh4944@gmail.com>
2026-03-27 23:34:33 +0100

    save

--- Day_25_CSV_Files_Pandas/main.py
+++ Day_25_CSV_Files_Pandas/main.py
@@ -11,10 +11,13 @@ turtle.shape(image)
 
 
 data = pandas.read_csv("./Day_25_CSV_Files_Pandas/50_states.csv")
+all_states = data["state"].to_list()
+
 
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").capitalize()
 
-if answer_state in data["state"]:
+
+if answer_state in all_states:
     print("yes")
 
 
