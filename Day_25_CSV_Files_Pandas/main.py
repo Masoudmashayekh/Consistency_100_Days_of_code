d234fafef6c952cef9c96336e97edf5ac1c85b81
Masoud <masoudmashayekh4944@gmail.com>
2026-03-27 23:29:16 +0100

    save

--- Day_25_CSV_Files_Pandas/main.py
+++ Day_25_CSV_Files_Pandas/main.py
@@ -1,11 +1,7 @@
 import turtle
 import pandas
+
 # Turtle just works with image.gif
-data = pandas.read_csv("./Day_25_CSV_Files_Pandas/50_states.csv")
-# df = data.data
-# print(df)
-
-
 
 screen = turtle.Screen()
 screen.title("U.S States Games")
@@ -13,8 +9,13 @@ image = "./Day_25_CSV_Files_Pandas/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+
+data = pandas.read_csv("./Day_25_CSV_Files_Pandas/50_states.csv")
+
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").capitalize()
-print(answer_state)
+
+if answer_state in data["state"]:
+    print("yes")
 
 
 
