589970dc93220f46c35d51aa35fc9db390061099
Masoud <masoudmashayekh4944@gmail.com>
2026-03-29 22:20:07 +0200

    save

--- Day_25_CSV_Files_Pandas/main.py
+++ Day_25_CSV_Files_Pandas/main.py
@@ -13,17 +13,18 @@ turtle.shape(image)
 data = pandas.read_csv("./Day_25_CSV_Files_Pandas/50_states.csv")
 all_states = data["state"].to_list()
 
+game_is_on = True
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").capitalize()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").capitalize()
 
-
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.penup()
-    t.hideturtle()
-    state_data = data[data.state == answer_state]
-    t.goto(state_data.x.item(), state_data.y.item())
-    t.write(state_data.state.item())
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.penup()
+        t.hideturtle()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(state_data.state.item())
 
 
 
