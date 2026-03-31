46e9bef730f2ef0757e2ad20c7545e2dddfa801c
Masoud <masoudmashayekh4944@gmail.com>
2026-03-31 12:41:38 +0200

    save

--- Day_26_List_Dictionary_Comprehensions/draft_1.py
+++ Day_26_List_Dictionary_Comprehensions/draft_1.py
@@ -1,1 +1,13 @@
 # Day 26 : List and Dictionary Comprehensions
+
+# numbers = [1, 2, 3]
+# new_list = []
+# for n in numbers:
+#     add_1  = n + 1
+#     new_list.append(add_1)
+
+# print(new_list)
+
+numbers = [1, 2, 3]
+new_list  = [ n+1 for n in numbers] # List Comprehension
+print(new_list)
