6bc0377bb7d8e68c5975853f4ad2d3ad75cffda8
Masoud <masoudmashayekh4944@gmail.com>
2026-04-05 15:00:26 +0200

    save

--- Day_26_List_Dictionary_Comprehensions/draft_1.py
+++ Day_26_List_Dictionary_Comprehensions/draft_1.py
@@ -32,3 +32,6 @@ names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
 
 short_names = [name for name in names if len(name) < 5]
 print(short_names)
+
+long_names = [name.upper() for name in names if len(name) > 5]
+print(long_names)
