203fff0a69d39722c9c9b1103cc96937292da2c1
Masoud <masoudmashayekh4944@gmail.com>
2026-04-10 15:48:28 +0200

    save

--- Day_26_List_Dictionary_Comprehensions/draft_1.py
+++ Day_26_List_Dictionary_Comprehensions/draft_1.py
@@ -41,3 +41,6 @@
 # Dictionary Comprehension
 # new_dict = {new_key: new_value for item in list}
 # new_dict = {new_key}: new_value for (key,value) in dict.items()}
+# new_dict = {new_key}: new_value for (key,value) in dict.items() if test}
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
