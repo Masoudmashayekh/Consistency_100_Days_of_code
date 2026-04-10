17de54136c405ccd8fc5f00e3206ec8f8b33f06b
Masoud <masoudmashayekh4944@gmail.com>
2026-04-10 16:06:30 +0200

    save

--- Day_26_List_Dictionary_Comprehensions/draft_1.py
+++ Day_26_List_Dictionary_Comprehensions/draft_1.py
@@ -47,3 +47,5 @@ import random
 names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
 students_scores = {student: random.randint(0, 100) for student in names}
 print(students_scores)
+passed_students = {student: score for (student,score) in students_scores.items() if score > 75}
+print(passed_students)
