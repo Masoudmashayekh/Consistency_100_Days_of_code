2a0430e99a631544b7be81694959dead5f284e3e
Masoud <masoudmashayekh4944@gmail.com>
2026-04-12 22:18:52 +0200

    save

--- Day_26_List_Dictionary_Comprehensions/draft_1.py
+++ Day_26_List_Dictionary_Comprehensions/draft_1.py
@@ -43,9 +43,19 @@
 # new_dict = {new_key: new_value for (key,value) in dict.items()}
 # new_dict = {new_key: new_value for (key,value) in dict.items() if test}
 
+#-------------------------------------------
+
 import random
 names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
 students_scores = {student: random.randint(0, 100) for student in names}
 print(students_scores)
 passed_students = {student: score for (student,score) in students_scores.items() if score > 75}
 print(passed_students)
+
+
+
+
+#-------------------------------------------
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word:len(word) for word in sentence.split()}
+print(result)
