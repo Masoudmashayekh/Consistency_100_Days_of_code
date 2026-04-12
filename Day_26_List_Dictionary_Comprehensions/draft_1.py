b1d648de36b1fa154d0c1260413b6f49a56c103e
Masoud <masoudmashayekh4944@gmail.com>
2026-04-12 22:13:53 +0200

    save

--- Day_26_List_Dictionary_Comprehensions/draft_1.py
+++ Day_26_List_Dictionary_Comprehensions/draft_1.py
@@ -40,8 +40,8 @@
 
 # Dictionary Comprehension
 # new_dict = {new_key: new_value for item in list}
-# new_dict = {new_key}: new_value for (key,value) in dict.items()}
-# new_dict = {new_key}: new_value for (key,value) in dict.items() if test}
+# new_dict = {new_key: new_value for (key,value) in dict.items()}
+# new_dict = {new_key: new_value for (key,value) in dict.items() if test}
 
 import random
 names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
