1c37e603fc8546a50da2e807cd3984876fccf03c
Masoud <masoudmashayekh4944@gmail.com>
2026-04-13 17:05:26 +0200

    save

--- Day_26_List_Dictionary_Comprehensions/main.py
+++ Day_26_List_Dictionary_Comprehensions/main.py
@@ -1,5 +1,5 @@
 import pandas
-
+# python3 main.py
 #TODO 1. Create a dictionary in this format:{"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
