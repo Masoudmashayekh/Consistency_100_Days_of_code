5fa6abb3f40610471e398c17a43f7a83786d0046
Masoud <masoudmashayekh4944@gmail.com>
2026-04-14 14:50:04 +0200

    save

--- Day_27_Tkinter_args_kwargs/draft_1.py
+++ Day_27_Tkinter_args_kwargs/draft_1.py
@@ -1,2 +1,19 @@
 # Day 27: Tkinter, *args, **kwargs and creating GUI programs
 # GUI : Graphical User Interface
+
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first GUI program")
+
+
+
+
+
+
+
+
+
+window.mainloop()
+    
+
