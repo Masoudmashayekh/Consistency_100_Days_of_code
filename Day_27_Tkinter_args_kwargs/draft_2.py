6ec80318d6154e74ad74166461a93768720c9e64
Masoud <masoudmashayekh4944@gmail.com>
2026-04-14 15:13:44 +0200

    save

--- Day_27_Tkinter_args_kwargs/draft_2.py
+++ Day_27_Tkinter_args_kwargs/draft_2.py
@@ -8,3 +8,24 @@
     
 # my_function(c= 3, a= 4, b= 5)
 #-------------------------------------------------------------------------
+# Arguments wiht Default Values : *args: Many positional arguments
+# def my_function(c= 3, a= 4, b= 5):
+#     Do this with a
+#     Then do this with b
+#     Finally do this with c
+    
+# my_function()
+# if I want to change something then:
+# my_function(b=8)
+#-------------------------------------------------------------------------
+# Unlimited Arguments
+def add(*args):
+    number = 0
+    for n in args:
+        number += n
+    return number
+        
+print(add(5,5,5))
+
+
+
