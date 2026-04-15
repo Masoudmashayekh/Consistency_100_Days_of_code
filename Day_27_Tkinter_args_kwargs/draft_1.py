b0a417d63aaf71d7e841434eb4ea0949e55d7a09
Masoud <masoudmashayekh4944@gmail.com>
2026-04-15 15:58:47 +0200

    save

--- Day_27_Tkinter_args_kwargs/draft_1.py
+++ Day_27_Tkinter_args_kwargs/draft_1.py
@@ -19,12 +19,11 @@ my_label.config(text="New Text 2")
 #------------------------------------------------------------------------------
 # Button:
 def button_clicked():
-    my_label.config(text="Button Got Clicked.")
+    new_text = input.get()
+    my_label.config(text= new_text)
 
-def show_input():
-    my_label.config(text=f"{input.get()}")
 
-button = tkinter.Button(text = "Click me!", command = show_input) # no need () in button_clicked function in command.
+button = tkinter.Button(text = "Click me!", command = button_clicked) # no need () in button_clicked function in command.
 button.pack()
 #------------------------------------------------------------------------------
 # Entry:
