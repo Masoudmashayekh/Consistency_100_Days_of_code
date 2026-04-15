b1b52993ddf6c8f4a89714e5474f83fe1f4c425a
Masoud <masoudmashayekh4944@gmail.com>
2026-04-15 15:40:39 +0200

    save

--- Day_27_Tkinter_args_kwargs/draft_1.py
+++ Day_27_Tkinter_args_kwargs/draft_1.py
@@ -12,7 +12,10 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a Label",font=("Arial", 14, "bold"))
 my_label.pack(side="left") # this line is important for showing my_label
 
+my_label["text"] = "New Text 1"
+my_label.config(text="New Text 2")
 
+# Button
 
 
 
