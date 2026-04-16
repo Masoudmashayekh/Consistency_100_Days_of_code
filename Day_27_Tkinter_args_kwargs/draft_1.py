a103f340fe45b832ae00d05b3d16988789581fc2
Masoud <masoudmashayekh4944@gmail.com>
2026-04-16 16:11:49 +0200

    save

--- Day_27_Tkinter_args_kwargs/draft_1.py
+++ Day_27_Tkinter_args_kwargs/draft_1.py
@@ -5,47 +5,30 @@
 import tkinter
 # from tkinter import *
 
-window = tkinter.Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-#------------------------------------------------------------------------------
-# Label:
-my_label = tkinter.Label(text="I am a Label",font=("Arial", 14, "bold"))
-my_label.pack() # this line is important for showing my_label
-
-my_label["text"] = "New Text 1"
-# or
-my_label.config(text="New Text 2")
-#------------------------------------------------------------------------------
-# Button:
 def button_clicked():
     new_text = input.get()
     my_label.config(text= new_text)
 
 
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+# Label:------------------------------------------------------------------------------
+my_label = tkinter.Label(text="I am a Label",font=("Arial", 14, "bold"))
+# my_label["text"] = "New Text 1"
+# or
+my_label.config(text="New Text 2")
+
+my_label.pack() # this line is important for showing my_label
+
+# Button:------------------------------------------------------------------------------
 button = tkinter.Button(text = "Click me!", command = button_clicked) # no need () in button_clicked function in command.
 button.pack()
-#------------------------------------------------------------------------------
-# Entry:
+
+# Entry:------------------------------------------------------------------------------
 input = tkinter.Entry(width= 10)
 input.pack()
-#------------------------------------------------------------------------------
-# Text
-
-#------------------------------------------------------------------------------
-# Spinbox
-
-#------------------------------------------------------------------------------
-# Scale
-
-#------------------------------------------------------------------------------
-# Checkbutton
-
-#------------------------------------------------------------------------------
-# Radiobutton
-
-#------------------------------------------------------------------------------
-# Listbox
 
 
 
