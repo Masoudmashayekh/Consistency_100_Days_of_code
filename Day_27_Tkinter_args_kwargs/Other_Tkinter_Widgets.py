9cb7dafcdad702856a4bfa043bfe8dd816ebfcc4
Masoud <masoudmashayekh4944@gmail.com>
2026-04-16 16:04:15 +0200

    save

--- Day_27_Tkinter_args_kwargs/Other_Tkinter_Widgets.py
+++ Day_27_Tkinter_args_kwargs/Other_Tkinter_Widgets.py
@@ -18,7 +18,7 @@ def action():
 button = Button(text="Click Me", command=action)
 button.pack()
 
-#Entries
+#Entries ------------------------------------------------------------------
 entry = Entry(width=30)
 #Add some text to begin with
 entry.insert(END, string="Some text to begin with.")
@@ -26,31 +26,31 @@ entry.insert(END, string="Some text to begin with.")
 print(entry.get())
 entry.pack()
 
-#Text
+#Text ------------------------------------------------------------------
 text = Text(height=5, width=30)
 #Puts cursor in textbox.
 text.focus()
 #Adds some text to begin with.
 text.insert(END, "Example of multi-line text entry.")
 #Get's current value in textbox at line 1, character 0
-print(text.get("1.0", END))
+print(text.get("1.0", END)) # 1 text starts at first line at charecter 0
 text.pack()
 
-#Spinbox
+#Spinbox ------------------------------------------------------------------
 def spinbox_used():
     #gets the current value in spinbox.
     print(spinbox.get())
 spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
 spinbox.pack()
 
-#Scale
+#Scale ------------------------------------------------------------------
 #Called with current scale value.
 def scale_used(value):
     print(value)
 scale = Scale(from_=0, to=100, command=scale_used)
 scale.pack()
 
-#Checkbutton
+#Checkbutton ------------------------------------------------------------------
 def checkbutton_used():
     #Prints 1 if On button checked, otherwise 0.
     print(checked_state.get())
@@ -60,7 +60,7 @@ checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbu
 checked_state.get()
 checkbutton.pack()
 
-#Radiobutton
+#Radiobutton ------------------------------------------------------------------
 def radio_used():
     print(radio_state.get())
 #Variable to hold on to which radio button value is checked.
@@ -71,7 +71,7 @@ radiobutton1.pack()
 radiobutton2.pack()
 
 
-#Listbox
+#Listbox ------------------------------------------------------------------
 def listbox_used(event):
     # Gets current selection from listbox
     print(listbox.get(listbox.curselection()))
