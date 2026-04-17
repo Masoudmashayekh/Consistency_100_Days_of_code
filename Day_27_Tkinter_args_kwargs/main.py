9a19ddeab4b4d48e38cb77ffecc7a0a89bec43a3
Masoud <masoudmashayekh4944@gmail.com>
2026-04-17 23:58:31 +0200

    save

--- Day_27_Tkinter_args_kwargs/main.py
+++ Day_27_Tkinter_args_kwargs/main.py
@@ -7,11 +7,12 @@ window.minsize(width= 300, height= 300)
 # Label ---------------------------------------
 label_equal = tkinter.Label(text= "is equal to: ", font=("Arial", 14, "bold"))
 label_equal.grid(row= 2, column= 1)
-label_equal.config(padx= 20, pady= 20)
+label_equal.config(padx= 10, pady= 10)
 
 # Label ---------------------------------------
-label_mlies_number = tkinter.Label(text= "0", font= ("Arial", 14, "bold"))
-label_mlies_number.grid(row= 1, column= 2)
+label_miles_number = tkinter.Label(text= "0", font= ("Arial", 14, "bold"))
+label_miles_number.grid(row= 1, column= 2)
+label_miles_number.config(padx= 10, pady= 10)
 
 # Label ---------------------------------------
 
