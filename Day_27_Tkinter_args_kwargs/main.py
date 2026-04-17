379aa05e6c8dd270f2aff02540ce74330e97e9bb
Masoud <masoudmashayekh4944@gmail.com>
2026-04-17 23:56:46 +0200

    save

--- Day_27_Tkinter_args_kwargs/main.py
+++ Day_27_Tkinter_args_kwargs/main.py
@@ -4,7 +4,20 @@ window = tkinter.Tk()
 window.title("Mile to Km Converter")
 window.minsize(width= 300, height= 300)
 
+# Label ---------------------------------------
+label_equal = tkinter.Label(text= "is equal to: ", font=("Arial", 14, "bold"))
+label_equal.grid(row= 2, column= 1)
+label_equal.config(padx= 20, pady= 20)
 
+# Label ---------------------------------------
+label_mlies_number = tkinter.Label(text= "0", font= ("Arial", 14, "bold"))
+label_mlies_number.grid(row= 1, column= 2)
+
+# Label ---------------------------------------
+
+# Label ---------------------------------------
+
+# Label ---------------------------------------
 
 
 
