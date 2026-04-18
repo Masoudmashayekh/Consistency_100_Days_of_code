97ce506bf977a750da43af468cef93ed7986b98d
Masoud <masoudmashayekh4944@gmail.com>
2026-04-18 23:44:22 +0200

    save

--- Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/main.py
+++ Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/main.py
@@ -20,21 +20,29 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx= 100, pady= 50, bg= YELLOW)
 
+# Label -----------------------------------------------------------------------------------------------------------
+label_timer = Label(text="Timer",fg= GREEN, bg= YELLOW, font=(FONT_NAME, 50, "bold"))
+label_timer.grid(row= 1, column= 2)
 
 
 
 
-
-
+# Canvas -----------------------------------------------------------------------------------------------------------
 canvas = Canvas(width= 200, height= 224, bg= YELLOW, highlightthickness= 0) # for put images in tkinter we should learn Canvas Widget
 tomato_img = PhotoImage(file="./Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/tomato.png")
 canvas.create_image(100, 112, image= tomato_img)
 canvas.create_text(100, 130, text= "00:00", fill="white",font= (FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row= 2, column= 2)
 
 
+# Button(Start) -----------------------------------------------------------------------------------------------------------
+button_start = Button(text= "Start", font= (FONT_NAME, 15, "bold"), bg= YELLOW, highlightthickness= 0)
+button_start.grid(row= 3, column= 1)
 
 
+# Button(Reset) -----------------------------------------------------------------------------------------------------------
+button_reset = Button(text= "Reset", font= (FONT_NAME, 15, "bold"), bg= YELLOW, highlightthickness= 0)
+button_reset.grid(row= 3, column= 3)
 
 
 
