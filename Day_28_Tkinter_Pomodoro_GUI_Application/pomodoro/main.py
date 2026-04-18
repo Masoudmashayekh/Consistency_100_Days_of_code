8a6409e4a300d00fc02293eab56ee2d9debf91c3
Masoud <masoudmashayekh4944@gmail.com>
2026-04-18 23:27:18 +0200

    save

--- Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/main.py
+++ Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/main.py
@@ -18,11 +18,13 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx= 100, pady= 50)
 
 
 canvas = Canvas(width= 200, height= 224) # for put images in tkinter we should learn Canvas Widget
 tomato_img = PhotoImage(file="./Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/tomato.png")
-canvas.create_image(100, 112, image= tomato_img)
+canvas.create_image(103, 112, image= tomato_img)
+canvas.create_text(103, 130, text= "00:00", fill="white",font= (FONT_NAME, 35, "bold"))
 canvas.pack()
 
 
