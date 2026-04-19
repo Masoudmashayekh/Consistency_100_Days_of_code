33ac950e8d1eeaed8b3dc6569fab36e01db87769
Masoud <masoudmashayekh4944@gmail.com>
2026-04-19 22:09:23 +0200

    save

--- Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/main.py
+++ Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/main.py
@@ -12,9 +12,13 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(10)
+    
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
+    canvas.itemconfig(timer_text, text= count)
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -34,8 +38,9 @@ label_timer.grid(row= 1, column= 2)
 canvas = Canvas(width= 200, height= 224, bg= YELLOW, highlightthickness= 0) # for put images in tkinter we should learn Canvas Widget
 tomato_img = PhotoImage(file="./Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/tomato.png")
 canvas.create_image(100, 112, image= tomato_img)
-canvas.create_text(100, 130, text= f"{count_down(10)}", fill="white",font= (FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text= "00:00", fill="white",font= (FONT_NAME, 35, "bold"))
 canvas.grid(row= 2, column= 2)
+
 
 
 # Button(Start) -----------------------------------------------------------------------------------------------------------
