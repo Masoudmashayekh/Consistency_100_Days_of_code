a9ab1c715c889ef12487bac8aee47d187be35a52
Masoud <masoudmashayekh4944@gmail.com>
2026-04-19 22:21:09 +0200

    save

--- Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/main.py
+++ Day_28_Tkinter_Pomodoro_GUI_Application/pomodoro/main.py
@@ -21,7 +21,11 @@ def start_timer():
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
-    canvas.itemconfig(timer_text, text= count)
+    if count_min < 10:
+        canvas.itemconfig(timer_text, text= f"0{count_min}:{count_sec}")
+    else:
+        canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}")
+    
     if count > 0:
         window.after(1000, count_down, count - 1)
 
