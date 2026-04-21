f0ff9818f9d1f5d4eaeb9048c0ea6eae51a3bba7
Masoud <masoudmashayekh4944@gmail.com>
2026-04-21 22:32:59 +0200

    save

--- Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/main.py
+++ Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/main.py
@@ -1,5 +1,20 @@
+from tkinter import *
+
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.minsize(width= 500, height= 500)
+
+
+
+
+
+
+window.mainloop()
+
