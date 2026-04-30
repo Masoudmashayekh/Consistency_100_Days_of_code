93e34bb06813f3bd6cc88dc0ec4a1962f264003c
Masoud <masoudmashayekh4944@gmail.com>
2026-04-30 23:38:44 +0200

    save

--- Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/main.py
+++ Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/main.py
@@ -13,8 +13,8 @@ window.minsize(width= 200, height= 200)
 window.config(padx= 20, pady= 20)
 
 canvas = Canvas(width= 200, height= 200, highlightthickness=0)
-luck_img = PhotoImage(file="./Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/logo.png")
-canvas.create_image(100, 100, image= luck_img)
+logo_img = PhotoImage(file="./Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/logo.png")
+canvas.create_image(100, 100, image= logo_img)
 canvas.pack()
 
 
