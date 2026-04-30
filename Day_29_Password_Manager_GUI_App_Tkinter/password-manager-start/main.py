1c146111c49f2f5296a458bd0cea51d8782cfaaa
Masoud <masoudmashayekh4944@gmail.com>
2026-05-01 00:06:59 +0200

    save

--- Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/main.py
+++ Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/main.py
@@ -15,31 +15,36 @@ window.config(padx= 20, pady= 20)
 canvas = Canvas(width= 200, height= 200, highlightthickness=0)
 logo_img = PhotoImage(file="./Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/logo.png")
 canvas.create_image(100, 100, image= logo_img)
-canvas.pack()
+canvas.grid(row= 1, column= 2)
 
 # Label: Website
 label_website = Label(text= "Website:", font= FONT)
-label_website.pack()
+label_website.grid(row= 2, column= 1)
 # Input: Website
 input_website = Entry(width= 35)
-input_website.pack()
+input_website.grid(row= 2, column= 2, columnspan= 2)
 
 
 # Label: Email / Username
-label_username = Label(text= "Email/username:")
-label_username.pack()
+label_username = Label(text= "Email/username:", font= FONT)
+label_username.grid(row= 3, column= 1)
 # Input: Email / Username
 input_username = Entry(width= 35)
-input_username.pack()
+input_username.grid(row= 3, column= 2, columnspan= 2)
 
 # Label: Password
-label_password = Label(text= "Password:")
-label_password.pack()
+label_password = Label(text= "Password:", font= FONT)
+label_password.grid(row= 4, column= 1)
 # Input: Password
 input_password = Entry(width= 21)
+input_password.grid(row= 4, column= 2)
 
 # Button: Generate Password
 button_password = Button(text= "Generate Password", command= "")
+button_password.grid(row= 4, column= 3)
+# Button: Add
+button_add = Button(text= "Add", width= 36, command= "")
+button_add.grid(row= 5, column= 2, columnspan= 2)
 
 
 
