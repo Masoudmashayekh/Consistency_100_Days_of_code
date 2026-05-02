75d7976546afcef568932e4b7c2b7b6a8a28bba4
Masoud <masoudmashayekh4944@gmail.com>
2026-05-02 23:54:59 +0200

    save

--- Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/main.py
+++ Day_29_Password_Manager_GUI_App_Tkinter/password-manager-start/main.py
@@ -8,30 +8,31 @@ FONT = ("Arial", 14, "bold")
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 #Password Generator Project
+def generate_password():
+    
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    password_list = []
 
-password_list = []
+    [password_list.append(random.choice(letters)) for char in range(nr_letters)]
 
+    [password_list.append(random.choice(symbols)) for char in range(nr_symbols)]
 
-[password_list.append(random.choice(letters)) for char in range(nr_letters)]
-
-[password_list.append(random.choice(symbols)) for char in range(nr_symbols)]
-
-[password_list.append(random.choice(numbers)) for char in range(nr_numbers)]
+    [password_list.append(random.choice(numbers)) for char in range(nr_numbers)]
 
 
-random.shuffle(password_list)
+    random.shuffle(password_list)
 
-password = "".join(password_list)
-
-print(f"Your password is: {password}")
+    password = "".join(password_list)
+    input_password.delete(first= 0, last= END)
+    input_password.insert(END, password)
+    # print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -87,7 +88,7 @@ input_password = Entry(width= 21)
 input_password.grid(row= 4, column= 2)
 
 # Button: Generate Password ----------------------
-button_password = Button(text= "Generate Password", command= "")
+button_password = Button(text= "Generate Password", command= generate_password)
 button_password.grid(row= 4, column= 3)
 # Button: Add -----------------------------------
 button_add = Button(text= "Add", width= 36, command= save_password)
