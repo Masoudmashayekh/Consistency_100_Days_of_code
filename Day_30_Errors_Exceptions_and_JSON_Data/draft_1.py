43f24ba47ba9575a87eb3f2eada76170f98306e2
Masoud <masoudmashayekh4944@gmail.com>
2026-05-03 22:51:12 +0200

    save

--- Day_30_Errors_Exceptions_and_JSON_Data/draft_1.py
+++ Day_30_Errors_Exceptions_and_JSON_Data/draft_1.py
@@ -4,9 +4,15 @@
 try:
     file = open("Day_30_Errors_Exceptions_and_JSON_Data/a_file.txt")
     a_dict = {"key": "value"}
-    print(a_dict["Hi"])
+    print(a_dict["key"])
 except FileNotFoundError:
     file = open("Day_30_Errors_Exceptions_and_JSON_Data/a_file.txt", "w")
     file.write("Something")
 except KeyError as error_message:
     print(f"That key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
