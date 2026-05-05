0ad2524c94ce117848bb565dec62ecc81ec8a40a
Masoud <masoudmashayekh4944@gmail.com>
2026-05-05 13:43:18 +0200

    save

--- Day_30_Errors_Exceptions_and_JSON_Data/NATO/main.py
+++ Day_30_Errors_Exceptions_and_JSON_Data/NATO/main.py
@@ -1,15 +1,19 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
+data = pandas.read_csv("Day_30_Errors_Exceptions_and_JSON_Data/NATO/nato_phonetic_alphabet.csv")
+
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
+go_on = True
+while go_on:
+    try:
+        word = input("Enter a word: ").upper()
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        go_on = False
+    
+
 print(output_list)
