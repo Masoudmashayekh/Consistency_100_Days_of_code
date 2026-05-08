841b70b10ea78621002ddd22c1e062e257497a6f
Masoud <masoudmashayekh4944@gmail.com>
2026-05-08 23:53:57 +0200

    save

--- Day_31_Flash_Card_App_Capstone_Project/main.py
+++ Day_31_Flash_Card_App_Capstone_Project/main.py
@@ -9,10 +9,13 @@ from tkinter import Canvas, PhotoImage
 
 window = tkinter.Tk()
 window.title("Flashy")
+window.config(padx= 50, pady= 50)
+
 
 canves = Canvas(height= 200, width= 200)
-fornt_img = PhotoImage(file= "Day_31_Flash_Card_App_Capstone_Project/images/card_front.png")
-
+front_img = PhotoImage(file= "Day_31_Flash_Card_App_Capstone_Project/images/card_front.png")
+canves.create_image(800, 526, image = front_img)
+canves.pack()
 
 
 
