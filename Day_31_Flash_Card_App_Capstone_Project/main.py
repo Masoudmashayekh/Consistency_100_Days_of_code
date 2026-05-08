ac75598ca488e117b677a447adec7a35103b613d
Masoud <masoudmashayekh4944@gmail.com>
2026-05-08 23:47:20 +0200

    save

---
+++ Day_31_Flash_Card_App_Capstone_Project/main.py
@@ -0,0 +1,21 @@
+BACKGROUND_COLOR = "#B1DDC6"
+
+                                            
+#----------------------------------------------
+import tkinter
+from tkinter import Canvas, PhotoImage
+
+
+
+window = tkinter.Tk()
+window.title("Flashy")
+
+canves = Canvas(height= 200, width= 200)
+fornt_img = PhotoImage(file= "Day_31_Flash_Card_App_Capstone_Project/images/card_front.png")
+
+
+
+
+
+
+window.mainloop()
