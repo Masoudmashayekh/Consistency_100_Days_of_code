d8573ad93cf8683ced214ce0dc1505b2da4f69f5
Masoud <masoudmashayekh4944@gmail.com>
2026-05-09 18:11:51 +0200

    save

--- Day_31_Flash_Card_App_Capstone_Project/main.py
+++ Day_31_Flash_Card_App_Capstone_Project/main.py
@@ -16,7 +16,7 @@ window.config(padx= 50, pady= 50, bg= BACKGROUND_COLOR)
 canves = Canvas(height=526, width= 800, bg= BACKGROUND_COLOR, highlightthickness= 0)
 card_front_img = PhotoImage(file= "Day_31_Flash_Card_App_Capstone_Project/images/card_front.png")
 canves.create_image(400, 263, image = card_front_img)
-title = canves.create_text(400, 150, text=f"Title", font=(FONT_NAME, 40, "italic"))
+title = canves.create_text(400, 130, text=f"Title", font=(FONT_NAME, 40, "italic"))
 word = canves.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
 canves.grid(row= 1, column= 1, columnspan= 2)
 
