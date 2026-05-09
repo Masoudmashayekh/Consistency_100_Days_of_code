5f2569011e03e53479a1b0181ebc09a3ff6a5f06
Masoud <masoudmashayekh4944@gmail.com>
2026-05-09 17:31:04 +0200

    save

--- Day_31_Flash_Card_App_Capstone_Project/main.py
+++ Day_31_Flash_Card_App_Capstone_Project/main.py
@@ -2,18 +2,18 @@ BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Courier"
                                             
 #----------------------------------------------
-import tkinter
-from tkinter import Canvas, PhotoImage
+from tkinter import *
 
 
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Flashy")
 window.minsize(height= 750, width=950)
-window.config(padx= 50, pady= 50)
+window.config(padx= 50, pady= 50, bg= BACKGROUND_COLOR)
 
 # Front --------------------------------------------------------
 canves = Canvas(height=526, width= 800)
+canves.config(bg= BACKGROUND_COLOR, highlightthickness= 0)
 front_img = PhotoImage(file= "Day_31_Flash_Card_App_Capstone_Project/images/card_front.png")
 canves.create_image(400, 270, image = front_img)
 canves.create_text(400, 100, text=f"English", font=(FONT_NAME, 30))
@@ -21,7 +21,12 @@ canves.create_text(400, 270, text="Word", font=(FONT_NAME, 50, "bold"))
 canves.pack()
 
 
+# Button True --------------------------------------------------------
+button_true = Button(text="")
+button_true.config(padx=30, pady=30,bg= BACKGROUND_COLOR, highlightthickness= 0)
+button_true.pack()
 
+# Button True --------------------------------------------------------
 
 
 window.mainloop()
