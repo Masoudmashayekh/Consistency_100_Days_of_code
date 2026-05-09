a1b6e8bf24e6413a1f223ff834beafc32338f415
Masoud <masoudmashayekh4944@gmail.com>
2026-05-09 17:13:58 +0200

    save

--- Day_31_Flash_Card_App_Capstone_Project/main.py
+++ Day_31_Flash_Card_App_Capstone_Project/main.py
@@ -1,5 +1,5 @@
 BACKGROUND_COLOR = "#B1DDC6"
-
+FONT_NAME = "Courier"
                                             
 #----------------------------------------------
 import tkinter
@@ -16,6 +16,8 @@ window.config(padx= 50, pady= 50)
 canves = Canvas(height=526, width= 800)
 front_img = PhotoImage(file= "Day_31_Flash_Card_App_Capstone_Project/images/card_front.png")
 canves.create_image(400, 270, image = front_img)
+canves.create_text(400, 100, text=f"English", font=(FONT_NAME, 30))
+canves.create_text(400, 270, text="Word", font=(FONT_NAME, 50, "bold"))
 canves.pack()
 
 
