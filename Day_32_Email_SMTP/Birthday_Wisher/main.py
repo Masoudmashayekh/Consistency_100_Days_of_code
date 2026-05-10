7955eae76f63175a44e65c3c7ce950a8f9150c27
Masoud <masoudmashayekh4944@gmail.com>
2026-05-10 18:00:53 +0200

    save

--- Day_32_Email_SMTP/Birthday_Wisher/main.py
+++ Day_32_Email_SMTP/Birthday_Wisher/main.py
@@ -1,8 +1,16 @@
-import os
-import dotenv
 import smtplib
+
+# Use .env -----------------------------------------------------
+import os
 from dotenv import load_dotenv
 load_dotenv()
+
+
+# Constants ---------------------------------------------------
 EMAIL = os.environ["MY_EMAIL"]
 print(EMAIL)
+
+
+
+
 # connection = smtplib.SMTP("smtp.gmail.com")
