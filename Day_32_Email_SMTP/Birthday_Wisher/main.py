95069f3de83c2f742d03e00c31a55b73a389fbb3
Masoud <masoudmashayekh4944@gmail.com>
2026-05-11 23:24:35 +0200

    save

--- Day_32_Email_SMTP/Birthday_Wisher/main.py
+++ Day_32_Email_SMTP/Birthday_Wisher/main.py
@@ -11,3 +11,5 @@ EMAIL = os.environ["MY_EMAIL"]
 
 # SMTP -------------------------------------------------------
 connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()  # For security resion
+connection.login(user= EMAIL, password=)
