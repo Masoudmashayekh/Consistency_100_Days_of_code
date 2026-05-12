9e066d44a8b494e2296ae2927639a0dbc6f5b02a
Masoud <masoudmashayekh4944@gmail.com>
2026-05-12 23:18:02 +0200

    save

--- Day_32_Email_SMTP/Birthday_Wisher/main.py
+++ Day_32_Email_SMTP/Birthday_Wisher/main.py
@@ -1,7 +1,12 @@
 ##################### Extra Hard Starting Project ######################
+import pandas
+
+
 
 # 1. Update the birthdays.csv
-
+data = pandas.read_csv("./Day_32_Email_SMTP/birthday_wisher/birthdays.csv")
+print(data)
+print(data["name"])
 # 2. Check if today matches a birthday in the birthdays.csv
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
