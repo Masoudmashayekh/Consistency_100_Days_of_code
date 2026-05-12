ecce7d9a32a7be62c0b67a761c99242887074c43
Masoud <masoudmashayekh4944@gmail.com>
2026-05-12 22:11:10 +0200

    save

--- Day_32_Email_SMTP/Birthday_Wisher/main.py
+++ Day_32_Email_SMTP/Birthday_Wisher/main.py
@@ -1,30 +1,33 @@
 import random
-# import smtplib
+import datetime as dt
+import smtplib
 
-# # Use .env -----------------------------------------------------
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
+# Use .env -----------------------------------------------------
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-# # Constants ---------------------------------------------------
-# MY_EMAIL = os.environ["MY_EMAIL"]
-# APP_PASSWORD = os.environ["APP_PASSWORD"]
-# CLIENT_EMAIL = os.environ["CLIENT_EMAIL"]
-# # SMTP -------------------------------------------------------
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()  # For security reasion
-#     connection.login(user= MY_EMAIL, password= APP_PASSWORD)
-#     connection.sendmail(
-#         from_addr= MY_EMAIL, 
-#         to_addrs= CLIENT_EMAIL, 
-#         msg="Subject: Hello \n\n This is body of my email."
-#         )
+# Constants ---------------------------------------------------
+MY_EMAIL = os.environ["MY_EMAIL"]
+APP_PASSWORD = os.environ["APP_PASSWORD"]
+CLIENT_EMAIL = os.environ["CLIENT_EMAIL"]
+# SMTP -------------------------------------------------------
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()  # For security reasion
+    connection.login(user= MY_EMAIL, password= APP_PASSWORD)
+    connection.sendmail(
+        from_addr= MY_EMAIL, 
+        to_addrs= CLIENT_EMAIL, 
+        msg="Subject: Hello \n\n This is body of my email."
+        )
 
 with open("Day_32_Email_SMTP/Birthday_Wisher/quotes.txt") as data:
    quotes = data.readlines()
-   quote = quotes[random.randint(len(quotes))]
-   print(quote)
-   
+   quote = random.choice(quotes)
+
+now = dt.datetime.now()
+day = now.weekday()
+
 
    
