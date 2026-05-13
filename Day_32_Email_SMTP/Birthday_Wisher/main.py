845cc776020ff5f4f5eaa73cd62338a39d28601f
Masoud <masoudmashayekh4944@gmail.com>
2026-05-13 22:42:49 +0200

    SAVE

--- Day_32_Email_SMTP/Birthday_Wisher/main.py
+++ Day_32_Email_SMTP/Birthday_Wisher/main.py
@@ -2,19 +2,33 @@
 import pandas
 import datetime as dt
 import random
+import smtplib
 
+
+# Use .env ------------------------------------------------------------------------------------
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Constants -----------------------------------------------------------------------------------
+MY_EMAIL = os.environ["MY_EMAIL"]
+APP_PASSWORD = os.environ["APP_PASSWORD"]
+
+
+# Date ----------------------------------------------------------------------------------------
 now = dt.datetime.now()
 day = now.day
 month = now.month
 
 
-# 1. Update the birthdays.csv
+# 1. Update the birthdays.csv -----------------------------------------------------------------
 data = pandas.read_csv("./Day_32_Email_SMTP/birthday_wisher/birthdays.csv")
-dic = data.to_dict(orient="records")
+data_dict = data.to_dict(orient="records")
 
 
-# 2. Check if today matches a birthday in the birthdays.csv
-for i in dic:
+# 2. Check if today matches a birthday in the birthdays.csv ------------------------------------
+for i in data_dict:
     if i["month"] == month and i["day"] == day:        
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
         with open(f"Day_32_Email_SMTP/birthday_wisher/letter_templates/letter_{random.randint(1,3)}.txt") as file:
@@ -22,7 +36,8 @@ for i in dic:
         new_letter = letter.replace("[NAME]", i["name"])
         print(new_letter)
 
-# 4. Send the letter generated in step 3 to that person's email address.
+
+# 4. Send the letter generated in step 3 to that person's email address. ------------------------
 
 
 
