f42bb5fb5472eb6b21ea50254e03224d1004b9e8
Masoud <masoudmashayekh4944@gmail.com>
2026-05-13 22:37:12 +0200

    save

--- Day_32_Email_SMTP/Birthday_Wisher/main.py
+++ Day_32_Email_SMTP/Birthday_Wisher/main.py
@@ -15,18 +15,13 @@ dic = data.to_dict(orient="records")
 
 # 2. Check if today matches a birthday in the birthdays.csv
 for i in dic:
-    if i["month"] == month and i["day"] == day:
-        print(i["name"])
-        
+    if i["month"] == month and i["day"] == day:        
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-with open(f"Day_32_Email_SMTP/birthday_wisher/letter_templates/letter_{random.randint(1,3)}.txt") as file:
-    letter = file.read()
-new_letter = letter.replace("[NAME]", "Masoud")
-print(new_letter)
+        with open(f"Day_32_Email_SMTP/birthday_wisher/letter_templates/letter_{random.randint(1,3)}.txt") as file:
+            letter = file.read()
+        new_letter = letter.replace("[NAME]", i["name"])
+        print(new_letter)
 
-# with open(f"Day_32_Email_SMTP/birthday_wisher/letter_templates/letter_masoud.txt", "w") as file:
-#     file.write(new_letter)
-# print(new_letter)
 # 4. Send the letter generated in step 3 to that person's email address.
 
 
