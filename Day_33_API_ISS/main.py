207720925a95697c5bd9cda9ea49a8c83fc84dc1
Masoud <masoudmashayekh4944@gmail.com>
2026-05-15 23:51:49 +0200

    save

--- Day_33_API_ISS/main.py
+++ Day_33_API_ISS/main.py
@@ -1,5 +1,7 @@
 import requests
 
+# Constants --------------------------------------------------------------
+
 # API -------------------------------------------------------------------
 response = requests.get(url= "http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -10,6 +12,6 @@ longitude = data["longitude"]
 latitude = data["latitude"]
 
 iss_position = (longitude, latitude)
-
+# https://www.latlong.net/
 
 print(iss_position)    
