e3b91c17f18cfcee2b30f928479c680b2bec463a
Masoud <masoudmashayekh4944@gmail.com>
2026-05-17 18:12:00 +0200

    save

--- Day_33_API_ISS/main.py
+++ Day_33_API_ISS/main.py
@@ -1,17 +1,22 @@
 import requests
+from datetime import datetime
 
 # Constants --------------------------------------------------------------
+MY_LAT = 45.176792
+MY_LONG = 9.192834
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
 
 # API -------------------------------------------------------------------
-response = requests.get(url= "http://api.open-notify.org/iss-now.json")
+response = requests.get("https://api.sunrise-sunset.org/json", params= parameters)
 response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) # 2026-05-17T03:49:34+00:00
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0]) # 2026-05-17T03:49:34+00:00
 
-data = response.json()["iss_position"]
-
-longitude = data["longitude"]
-latitude = data["latitude"]
-
-iss_position = (longitude, latitude)
-# https://www.latlong.net/
-
-print(iss_position)    
+time_now = datetime.now()
