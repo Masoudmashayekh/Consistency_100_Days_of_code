7b945e0c4d00ef12e3ba31fb8977dc4c54c852be
Masoud <masoudmashayekh4944@gmail.com>
2026-05-19 23:43:10 +0200

    save

--- Day_34_API_Practice/quizzler-app-start/data.py
+++ Day_34_API_Practice/quizzler-app-start/data.py
@@ -5,6 +5,7 @@ parameters = {
     "type": "boolean"
 }
 
+
 # url="https://opentdb.com/api.php?amount=10&type=boolean"
 response = requests.get(url="https://opentdb.com/api.php", params= parameters)
 response.raise_for_status()
