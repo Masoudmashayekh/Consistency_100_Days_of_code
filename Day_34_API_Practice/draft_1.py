e50c0c0a74081fc5e192c9410fb43c3340392a40
Masoud <masoudmashayekh4944@gmail.com>
2026-05-22 23:12:51 +0200

    save

--- Day_34_API_Practice/draft_1.py
+++ Day_34_API_Practice/draft_1.py
@@ -1,4 +1,12 @@
 # Section 34: Day 34 - Intermediate+ API Practice - Creating a GUI Quiz App
 
 
-import requests
+# import requests
+
+
+
+
+# class Quiz:
+    
+#     def __init__(self):
+#         self.window = Tk()
