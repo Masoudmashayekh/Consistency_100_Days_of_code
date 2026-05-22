75f73889706450eef311d49ead5307a58b26174f
Masoud <masoudmashayekh4944@gmail.com>
2026-05-22 23:25:30 +0200

    save

--- Day_34_API_Practice/quizzler-app-start/ui.py
+++ Day_34_API_Practice/quizzler-app-start/ui.py
@@ -7,8 +7,18 @@ THEME_COLOR = "#375362"
 class QuizInterface:
     
     def  __init__(self):
+        self.score = 0
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.configure(bg= THEME_COLOR, height= 250, width= 300)
+        self.label = Label(text= "score")
+        self.label.pack()
+        
+        
+        
+        
+        
+        
         
         self.window.mainloop()
         
