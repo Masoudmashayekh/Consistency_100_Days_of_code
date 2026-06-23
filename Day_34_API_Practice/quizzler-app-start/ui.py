8365f2ec2533b19172b6790fd0c079c704372a06
Masoud <masoudmashayekh4944@gmail.com>
2026-06-23 16:26:01 +0200

    save

--- Day_34_API_Practice/quizzler-app-start/ui.py
+++ Day_34_API_Practice/quizzler-app-start/ui.py
@@ -18,7 +18,7 @@ class QuizInterface:
         self.score_label.grid(row=1, column= 2)   
         
         self.canvas = Canvas(width= 300, height= 250, bg= "white")
-        self.question_text = self.canvas.create_text(150, 125, text= "Some question", fill= THEME_COLOR)
+        self.question_text = self.canvas.create_text(150, 125, text= "Some question", fill= THEME_COLOR,font=("Arial", 20, "italic") )
         self.canvas.grid(row= 2, column= 1, columnspan= 2)
 
 
