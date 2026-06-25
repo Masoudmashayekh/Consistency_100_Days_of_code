46decdebea52710f2c11a02964d6621168d491ca
Masoud <masoudmashayekh4944@gmail.com>
2026-06-25 14:55:09 +0200

    save

--- Day_34_API_Practice/quizzler-app-start/ui.py
+++ Day_34_API_Practice/quizzler-app-start/ui.py
@@ -23,11 +23,11 @@ class QuizInterface:
         self.canvas.grid(row= 2, column= 1, columnspan= 2, pady= 50) 
 
         true_image = PhotoImage(file="Day_34_API_Practice/quizzler-app-start/images/true.png")
-        self.true_button = Button(image= true_image, highlightthickness=0)
+        self.true_button = Button(image= true_image, highlightthickness=0, command= self.true_pressed)
         self.true_button.grid(row= 3, column= 1)
         
         false_image = PhotoImage(file="Day_34_API_Practice/quizzler-app-start/images/false.png")
-        self.false_button = Button(image= false_image, highlightthickness= 0)
+        self.false_button = Button(image= false_image, highlightthickness= 0, compound= self.false_pressed)
         self.false_button.grid(row= 3, column= 2)
         
         self.get_next_question()
@@ -37,3 +37,10 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text= q_text)
+        
+        
+    def true_pressed(self):
+        pass
+    
+    def false_pressed(self):
+        pass
